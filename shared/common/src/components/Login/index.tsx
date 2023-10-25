30587d1bf25fe3e86ed73d67fbe370a2ff2ea775
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-10-25 19:41:29 +0900

    Update test1

---
+++ shared/common/src/components/Login/index.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import * as S from "./style";
+
+const Login = () => {
+    return(
+        <S.LoginWrapper>
+            asdasdasdasdasd
+        </S.LoginWrapper>
+    );
+}
+
+export default Login;
---
+++ shared/common/src/components/Login/style.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const LoginWrapper = styled.div`
+    width: 432px;
+    height: 531px;
+    border-radius: 8px;
+    border: 1px solid #b8b8b8;
+`
