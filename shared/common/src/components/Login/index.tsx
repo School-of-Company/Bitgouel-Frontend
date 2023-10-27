99ecd1c228777faaa5629f181cccb4ae79d4278a
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-10-28 03:23:41 +0900

    Update login component publishing

--- shared/common/src/components/Login/index.tsx
+++ shared/common/src/components/Login/index.tsx
@@ -1,13 +1,44 @@
 'use client'
 
-import * as S from "./style";
+import * as S from './style'
+import ValueInput from '../ValueInput'
 
 const Login = () => {
-    return(
-        <S.LoginWrapper>
-            asdasdasdasdasd
-        </S.LoginWrapper>
-    );
+  return (
+    <S.LoginWrapper>
+      <S.TitleWrapper>
+        <S.TitleContainer>
+          <S.TitleItemWrapper>
+            <S.TitleItem>빛고을</S.TitleItem>
+            <S.TitleItem>직업교육</S.TitleItem>
+            <S.TitleItem>혁신지구</S.TitleItem>
+          </S.TitleItemWrapper>
+        </S.TitleContainer>
+      </S.TitleWrapper>
+      <S.InputWrapper>
+        <S.InputContainer>
+          <ValueInput placeholder='이메일' />
+          <ValueInput placeholder='비밀번호' />
+        </S.InputContainer>
+        <S.PasswordContainer>
+          <S.MenuItem>비밀번호를 잊었나요?</S.MenuItem>
+          <S.PasswordSearch>비밀번호 찾기</S.PasswordSearch>
+        </S.PasswordContainer>
+      </S.InputWrapper>
+      <S.LoginButtonWrapper>
+        <S.LoginButton>로그인</S.LoginButton>
+      </S.LoginButtonWrapper>
+      <S.JoinWrapper>
+        <S.JoinContainer>
+          <S.MenuItem>또는</S.MenuItem>
+          <div>
+            <S.NoAccountItem>계정이 없으신가요?</S.NoAccountItem>
+            <S.UserJoinItem>회원가입</S.UserJoinItem>
+          </div>
+        </S.JoinContainer>
+      </S.JoinWrapper>
+    </S.LoginWrapper>
+  )
 }
 
-export default Login;
+export default Login
--- shared/common/src/components/Login/style.ts
+++ shared/common/src/components/Login/style.ts
@@ -1,8 +1,109 @@
-import styled from "@emotion/styled";
+import { theme } from '../../../../styles/theme'
+import styled from '@emotion/styled'
 
 export const LoginWrapper = styled.div`
-    width: 432px;
-    height: 531px;
-    border-radius: 8px;
-    border: 1px solid #b8b8b8;
+  width: 432px;
+  height: 531px;
+  border-radius: 8px;
+  border: 1px solid #b8b8b8;
+`
+
+export const TitleWrapper = styled.div`
+  width: 100%;
+  height: 177px;
+`
+export const TitleItemWrapper = styled.div`
+  padding: 24px 0 0 24px;
+  display: flex;
+  flex-direction: column;
+`
+
+export const TitleContainer = styled.div`
+  width: 216px;
+  height: 177px;
+`
+
+export const TitleItem = styled.span`
+  ${theme.typo.title_md};
+`
+
+export const InputWrapper = styled.div`
+  width: 100%;
+  display: flex;
+
+  align-items: center;
+  flex-direction: column;
+`
+
+export const InputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+`
+
+export const PasswordContainer = styled.div`
+  width: 88%;
+  display: flex;
+  gap: 210px;
+  margin-top: 4px;
+`
+
+export const MenuItem = styled.span`
+  ${theme.typo.caption};
+  color: ${theme.color.gray['400']};
+  cursor: pointer;
+`
+
+export const PasswordSearch = styled.span`
+  ${theme.typo.caption};
+  color: ${theme.color.main};
+  cursor: pointer;
+`
+
+export const LoginButtonWrapper = styled.div`
+  width: 100%;
+  height: 106px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`
+
+export const LoginButton = styled.button`
+  background-color: ${theme.color.main};
+  color: ${theme.color.white};
+  ${theme.typo.text_lg};
+  width: 384px;
+  height: 52px;
+  border-radius: 8px;
+  border: 0;
+  cursor: pointer;
+`
+
+export const JoinWrapper = styled.div`
+  width: 100%;
+  height: 95px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const JoinContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 8px;
+`
+
+export const NoAccountItem = styled.span`
+  ${theme.typo.text_md};
+  color: ${theme.color.black};
+`
+
+export const UserJoinItem = styled.span`
+  ${theme.typo.text_md};
+  color: ${theme.color.main};
+  margin-left: 4px;
+  cursor: pointer;
 `
