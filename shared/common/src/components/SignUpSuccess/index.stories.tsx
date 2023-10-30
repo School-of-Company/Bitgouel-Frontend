00dc12c873f6c04e428ef3db44f5611fb152c9c3
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-10-30 10:35:27 +0900

    Add signUpSuccess component

---
+++ shared/common/src/components/SignUpSuccess/index.stories.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import SignUpSuccess from '.'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'common/SignUpSuccess',
+  component: SignUpSuccess,
+} as Meta<typeof SignUpSuccess>
+
+type Story = StoryObj<typeof SignUpSuccess>
+
+export const Primary: Story = {}
