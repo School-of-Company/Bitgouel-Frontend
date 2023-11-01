3c47753c9342e03e6c4af83ff03f407ad9a32848
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-11-01 22:48:38 +0900

    Update XIcon active

--- shared/common/src/components/ValueInput/index.tsx
+++ shared/common/src/components/ValueInput/index.tsx
@@ -1,6 +1,7 @@
-import React, { forwardRef, InputHTMLAttributes } from 'react'
+import React, { forwardRef, InputHTMLAttributes, useEffect } from 'react'
 import * as S from './style'
 import { XIcon } from '../../assets'
+import { useState } from 'react'
 
 interface ValueInputProps extends InputHTMLAttributes<HTMLInputElement> {
   onClear: () => void
@@ -11,10 +12,21 @@ const ValueInput = (
   { length, onClear, ...rest }: ValueInputProps,
   ref?: any
 ) => {
+  const [focus, setFocus] = useState<boolean>(true)
+
   return (
-    <S.ValueWrapper>
+    <S.ValueWrapper
+      onMouseOver={() => {
+        setFocus(true)
+        console.log(focus)
+      }}
+      onMouseOut={() => {
+        setFocus(false)
+        console.log(focus)
+      }}
+    >
       <S.ValueInput ref={ref} {...rest} />
-      {length > 0 && (
+      {length > 0 && focus === true && (
         <S.XIconWrapper onClick={onClear}>
           <XIcon />
         </S.XIconWrapper>
