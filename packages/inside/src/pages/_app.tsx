2eea6287bd0e1501c931e1214edc0a513f97dce6
unrin <momlin069@gmail.com>
2023-11-06 15:19:50 +0900

    Update apply globals.css

--- packages/inside/src/pages/_app.tsx
+++ packages/inside/src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import type { AppProps } from 'next/app'
 import { GlobalLayout } from '@common/layouts'
-import '@common/styles/globals.css'
-import '@/styles/font.css'
 import { useRecoilValue } from 'recoil'
 import { IsModal } from '@common/atoms'
+import '@common/styles/globals.css'
+import '@/styles/font.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   const isModal = useRecoilValue(IsModal)
--- packages/outside/src/pages/_app.tsx
+++ packages/outside/src/pages/_app.tsx
@@ -2,10 +2,12 @@ import type { AppProps } from 'next/app'
 import { GlobalLayout } from '@common/layouts'
 import { useRecoilValue } from 'recoil'
 import { IsModal } from '@common/atoms'
+import '@common/styles/globals.css'
 import '@/styles/font.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   const isModal = useRecoilValue(IsModal)
+
   return (
     <GlobalLayout>
       <Component {...pageProps} />
