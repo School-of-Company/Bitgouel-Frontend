a9ed3a6a13703bd23c574f2eed71731aa21a13b0
송현우 <shw9052@naver.com>
2023-11-06 14:41:50 +0900

    Add notice page

---
+++ shared/common/src/pages/notice/index.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import * as S from './style'
+import { Header } from '../../components'
+import Bg1 from '../../assets/png/mainBg1.png'
+import { MegaPhone, Message, Question } from '../../assets/'
+
+const NoticePage = () => {
+  return (
+    <S.NoticeWrraper>
+      <Header />
+      <S.SlideBg url={Bg1}>
+        <S.BgContainer>
+          <span>게시글</span>
+          <S.ButtonContainer>
+            <S.LectureButton>
+              <MegaPhone />
+              <span>공지사항</span>
+            </S.LectureButton>
+            <S.LectureButton>
+              <Message />
+              <span>취업 게시글</span>
+            </S.LectureButton>
+            <S.LectureButton>
+              <Question />
+              <span>문의사항</span>
+            </S.LectureButton>
+          </S.ButtonContainer>
+        </S.BgContainer>
+      </S.SlideBg>
+    </S.NoticeWrraper>
+  )
+}
+
+export default NoticePage
