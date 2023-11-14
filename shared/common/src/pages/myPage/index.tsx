0ddc4c060ad3366c3d4e4993d864f8e109392ff1
송현우 <shw9052@naver.com>
2023-11-14 23:28:38 +0900

    Add myPage

--- shared/common/src/pages/myPage/index.tsx
+++ shared/common/src/pages/myPage/index.tsx
@@ -5,7 +5,52 @@ const MyPage = () => {
   return (
     <S.MyPageWrapper url={Bg4}>
       <S.BlackBox>
-        
+        <S.WhiteBox>
+          <S.MyIdentify>
+            <S.ProfileContainer>
+              <div>
+                <S.Name>홍길동</S.Name>
+                <S.Role>학생</S.Role>
+              </div>
+              <div>
+                <S.SchoolName>광주 소프트웨어 마이스터 고등학교</S.SchoolName>
+                <S.SubEnter>소속</S.SubEnter>
+              </div>
+            </S.ProfileContainer>
+            <S.AccountWrapper>
+              <S.MyTitle>계정 정보</S.MyTitle>
+              <S.AccountContainer>
+                <div>
+                  <S.LeftText>bitgoeul@gmail.com</S.LeftText>
+                  <S.RightText>이메일</S.RightText>
+                </div>
+                <div>
+                  <S.LeftText>010-0000-0000</S.LeftText>
+                  <S.RightText>전화번호</S.RightText>
+                </div>
+                <div>
+                  <S.LeftText>N점</S.LeftText>
+                  <S.RightText>학점</S.RightText>
+                </div>
+              </S.AccountContainer>
+            </S.AccountWrapper>
+            <S.SharedLine />
+            <S.AccountSettingWrapper>
+              <S.MyTitle>계정 설정</S.MyTitle>
+              <S.AccountSettingContainer>
+                <div>
+                  <S.LeftText>회원정보 수정</S.LeftText>
+                  <S.ModifyText>비밀번호 수정</S.ModifyText>
+                </div>
+                <S.SharedLine />
+                <div>
+                  <S.LeftText>회원 탈퇴</S.LeftText>
+                  <S.WithDrow>회원 탈퇴</S.WithDrow>
+                </div>
+              </S.AccountSettingContainer>
+            </S.AccountSettingWrapper>
+          </S.MyIdentify>
+        </S.WhiteBox>
       </S.BlackBox>
     </S.MyPageWrapper>
   )
--- shared/common/src/pages/myPage/style.ts
+++ shared/common/src/pages/myPage/style.ts
@@ -3,14 +3,116 @@ import styled from '@emotion/styled'
 export const MyPageWrapper = styled.div<{ url: any }>`
   background-image: url(${({ url }) => url.src});
   position: absolute;
+  display: flex;
+  justify-content: center;
   height: 100%;
   width: 100%;
   background-position: center;
   background-size: cover;
+  overflow: hidden;
 `
 
 export const BlackBox = styled.div`
-    width: 636px;
-    height: 920px;
-    background-color: ba;
+  display: flex;
+  justify-content: center;
+  padding-top: 1.5rem;
+  margin-top: 7.375rem;
+  width: 39.75rem;
+  height: 57.5rem;
+  background-color: ${({ theme }) => theme.color.gray[200]};
+  border-radius: 0.5rem;
+`
+
+export const WhiteBox = styled.div`
+  width: 36.75rem;
+  height: 52.5rem;
+  border-radius: 0.5rem;
+  background-color: ${({ theme }) => theme.color.white};
+`
+
+export const MyIdentify = styled.div`
+  margin: 2.5rem 1.5rem;
+`
+
+export const ProfileContainer = styled.div``
+
+export const Name = styled.span`
+  color: ${({ theme }) => theme.color.black};
+  ${({ theme }) => theme.typo.title_lg};
+  margin-right: 0.8rem;
+`
+
+export const Role = styled.span`
+  color: ${({ theme }) => theme.color.gray[700]};
+  ${({ theme }) => theme.typo.title_sm}
+`
+
+export const SchoolName = styled.span`
+  color: ${({ theme }) => theme.color.gray[400]};
+  ${({ theme }) => theme.typo.text_lg}
+  margin-top: 0.25rem;
+  margin-right: 0.8rem;
+`
+
+export const SubEnter = styled.span`
+  color: ${({ theme }) => theme.color.gray[700]};
+  ${({ theme }) => theme.typo.text_lg};
+`
+
+export const AccountWrapper = styled.div`
+  margin: 2.5rem 0;
+`
+
+export const MyTitle = styled.span`
+  ${({ theme }) => theme.typo.title_sm};
+`
+
+export const AccountContainer = styled.div`
+  margin-top: 1rem;
+  div {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.5rem;
+  }
+`
+
+export const LeftText = styled.span`
+  color: ${({ theme }) => theme.color.gray[400]};
+  ${({ theme }) => theme.typo.text_md};
+`
+
+export const RightText = styled.span`
+  color: ${({ theme }) => theme.color.gray[700]};
+  ${({ theme }) => theme.typo.text_md};
+`
+
+export const SharedLine = styled.div`
+  width: 100%;
+  height: 0.0625rem;
+  background-color: ${({ theme }) => theme.color.gray[900]};
+`
+
+export const AccountSettingWrapper = styled.div`
+  margin-top: 5rem;
+`
+
+export const AccountSettingContainer = styled.div`
+  margin-top: 2.5rem;
+  div {
+    display: flex;
+    justify-content: space-between;
+    margin: 1.5rem 0rem;
+  }
+`
+
+export const ModifyText = styled.span`
+  color: ${({ theme }) => theme.color.main};
+  text-decoration: underline;
+  cursor: pointer;
+`
+
+export const WithDrow = styled.span`
+  color: ${({ theme }) => theme.color.error};
+  text-decoration: underline;
+  cursor: pointer;
 `
