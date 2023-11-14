14364c560ae1d9a05bdc0cddf260471a6fd2b8b3
TaerangLee <s22051@gsm.hs.kr>
2023-11-14 09:35:10 +0900

    Add export lecture query settings

--- shared/api/common/src/hooks/lecture/useGetDetailLecture.ts
+++ shared/api/common/src/hooks/lecture/useGetDetailLecture.ts
@@ -1,24 +1,24 @@
-import { useQuery, UseQueryOptions } from "@tanstack/react-query";
-import { lectureQueryKeys } from "../../libs/queryKeys";
-import { lectureUrl } from "../../libs/urlController";
-import { get } from "../../libs";
-import { AxiosResponse } from "axios";
+import { useQuery, UseQueryOptions } from '@tanstack/react-query'
+import { lectureQueryKeys } from '../../libs/queryKeys'
+import { lectureUrl } from '../../libs/urlController'
+import { get } from '../../libs'
+import { AxiosResponse } from 'axios'
 
 interface LectureDetailItemType {
-  id: string;
-  name: string;
-  content: string;
-  startDate: string;
-  endDate: string;
-  completeDate: string;
-  lectureType: string;
-  lectureStatus: string;
-  headCount: number;
-  maxRegisteredUser: number;
-  lecturer: string;
+  id: string
+  name: string
+  content: string
+  startDate: string
+  endDate: string
+  completeDate: string
+  lectureType: string
+  lectureStatus: string
+  headCount: number
+  maxRegisteredUser: number
+  lecturer: string
 }
 
-export const useGetLectureList = (
+export const useGetDetailLecture = (
   id: string,
   options?: UseQueryOptions<AxiosResponse<LectureDetailItemType[]>>
 ) =>
@@ -26,4 +26,4 @@ export const useGetLectureList = (
     lectureQueryKeys.getLectureDetail(id),
     () => get(lectureUrl.lectureDetail(id)),
     options
-  );
+  )
