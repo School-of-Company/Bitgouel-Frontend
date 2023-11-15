93bd65d77124e75ecb8a5b2bf1d5e57ba2d3dd97
송현우 <shw9052@naver.com>
2023-11-15 19:45:57 +0900

    Add item map

--- shared/common/src/components/Header/index.tsx
+++ shared/common/src/components/Header/index.tsx
@@ -1,166 +1,27 @@
 'use client'
 
+import * as S from './style'
 import Symbol1 from '../../assets/png/symbol1.png'
 import Symbol2 from '../../assets/png/symbol2.png'
-import * as S from './style'
-import { match } from 'ts-pattern'
-import { usePathname, useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
-import { Plus, Filter, MegaPhone, Message, Question } from '../../assets'
-import { LectureTypeModal } from '../../modals'
-import { useRecoilState } from 'recoil'
-import { LectureTypeText } from '../../atoms'
 
-const Header = ({ inside }: { inside: boolean }) => {
-  const router = useRouter()
-  const pathname = usePathname()
+const Header = () => {
   const menuList = [
-    { kor: '사업소개', link: '/' },
-    { kor: '강의', link: '/main/lecture' },
-    { kor: '동아리', link: '/main/club' },
-    { kor: '게시글', link: '/main/notice' },
+    { kor: '사업소개' },
+    { kor: '강의' },
+    { kor: '동아리' },
+    { kor: '게시글' },
   ]
 
-  const [bgColor, setBgColor] = useState<string>('')
-  const [symbolNum, setSymbolNum] = useState<any>(Symbol1)
-  const [btnColor, setBtnColor] = useState<string>('rgb(255, 255, 255, 0.2)')
-  const [borderColor, setBorderColor] = useState<string>('')
-  const [spanColor, setSpanColor] = useState<string>('#fff')
-  const [svgView, setSvgView] = useState<string>('none')
-  const [myStatus, setMyStatus] = useState<string>('로그인')
-  const [isLectureType, setIsLectureType] = useState<boolean>(false)
-  const [lectureTypeText, setLectureTypeText] =
-    useRecoilState<string>(LectureTypeText)
-
-  useEffect(() => {
-    if (localStorage.getItem('Bitgouel-accessToken')) {
-      if (pathname === '/main/myPage') {
-        setMyStatus('로그아웃')
-      } else {
-        setMyStatus('내 정보')
-      }
-    } else {
-      setMyStatus('로그인')
-    }
-
-    const onScroll = () => {
-      const { scrollY } = window
-      if (pathname === '/main/home') {
-        if (scrollY >= 800) {
-          setBgColor('#fff')
-          setSymbolNum(Symbol2)
-          setBtnColor('rgb(209, 209, 209, 1)')
-          setBorderColor('0.0625rem solid #ebebeb')
-          setSpanColor('#288BE1')
-        } else {
-          setBgColor('')
-          setSymbolNum(Symbol1)
-          setBtnColor('rgb(255, 255, 255, 0.2)')
-          setBorderColor('')
-          setSpanColor('#fff')
-        }
-      } else {
-        if (scrollY >= 240) {
-          setBgColor('#fff')
-          setSymbolNum(Symbol2)
-          setBtnColor('rgb(209, 209, 209, 1)')
-          setBorderColor('0.0625rem solid #ebebeb')
-          setSpanColor('#288BE1')
-          setSvgView('flex')
-        } else {
-          setBgColor('')
-          setSymbolNum(Symbol1)
-          setBtnColor('rgb(255, 255, 255, 0.2)')
-          setBorderColor('')
-          setSpanColor('#fff')
-          setSvgView('none')
-        }
-      }
-    }
-    window.addEventListener('scroll', onScroll)
-    return () => {
-      window.removeEventListener('scroll', onScroll)
-    }
-  }, [])
-
   return (
-    <S.HeaderWrapper
-      bgColor={bgColor}
-      borderColor={borderColor}
-      isAuth={match(pathname)
-        .with('/auth/login', () => true)
-        .with('/auth/signUp', () => true)
-        .otherwise(() => false)}
-    >
-      <S.HeaderContainer>
-        <S.SymbolContainer url={symbolNum} />
-        <S.MenuWrapper>
+    <S.HeaderWrapper>
+      <S.MenuContainer>
+        <S.Symbol url={Symbol1} />
+        <S.MenuList>
           {menuList.map((menu, idx) => (
-            <S.MenuItem
-              key={idx}
-              onClick={() =>
-                (myStatus === '내 정보' || myStatus === '로그아웃') &&
-                router.push(menu.link)
-              }
-              isSameRoute={pathname === menu.link}
-              color={spanColor}
-            >
-              {menu.kor}
-            </S.MenuItem>
+            <S.MenuItem key={idx}>{menu.kor}</S.MenuItem>
           ))}
-        </S.MenuWrapper>
-        <S.ButtonWrapper view={svgView}>
-          {match(pathname)
-            .with('/main/lecture', () => (
-              <S.CreateIcon
-                onClick={() => {
-                  router.push('/main/lecture/create')
-                }}
-                view={match(inside)
-                  .with(true, () => 'none')
-                  .otherwise(() => '')}
-              >
-                <Plus />
-              </S.CreateIcon>
-            ))
-            .with('/main/notice', () => <MegaPhone />)
-            .otherwise(() => null)}
-          {match(pathname)
-            .with('/main/lecture', () => (
-              <S.SelectFilterContainer>
-                <div onClick={() => setIsLectureType((prev) => !prev)}>
-                  <Filter />
-                </div>
-                {isLectureType && (
-                  <LectureTypeModal
-                    location='헤더'
-                    text={lectureTypeText}
-                    setText={setLectureTypeText}
-                    setIsLectureType={setIsLectureType}
-                  />
-                )}
-              </S.SelectFilterContainer>
-            ))
-            .with('/main/notice', () => <Message />)
-            .otherwise(() => null)}
-          {match(pathname)
-            .with('/main/notice', () => <Question />)
-            .otherwise(() => null)}
-        </S.ButtonWrapper>
-        <S.LoginButton
-          onClick={() =>
-            match(myStatus)
-              .with('내 정보', () => router.push('/main/myPage'))
-              .with('로그아웃', () => {
-                router.push('/auth/login'), localStorage.clear()
-              })
-              .otherwise(() => router.push('/auth/login'))
-          }
-          color={btnColor}
-        >
-          <span>{myStatus}</span>
-        </S.LoginButton>
-      </S.HeaderContainer>
+        </S.MenuList>
+      </S.MenuContainer>
     </S.HeaderWrapper>
   )
 }
--- shared/common/src/components/Header/style.ts
+++ shared/common/src/components/Header/style.ts
@@ -1,97 +1,23 @@
 import styled from '@emotion/styled'
 
-export const HeaderWrapper = styled.div<{
-  bgColor: string
-  borderColor: string
-  isAuth: boolean
-}>`
+export const HeaderWrapper = styled.div`
+  background-color: red;
   width: 100%;
   height: 4.875rem;
-  display: ${({ isAuth }) => (isAuth ? 'none' : 'flex')};
-  position: fixed;
-  justify-content: center;
-  top: 0;
-  transition: all 0.5s;
-  background-color: ${({ bgColor }) => bgColor};
-  border-bottom: ${({ borderColor }) => borderColor};
-  z-index: 999;
 `
-
-export const HeaderContainer = styled.div`
+export const MenuContainer = styled.div`
   display: flex;
-  width: 75rem;
-  height: 100%;
-  align-items: center;
-  justify-content: space-between;
+  background-color: blue;
 `
 
-export const SymbolContainer = styled.div<{ url: any }>`
+export const Symbol = styled.div<{ url: any }>`
   width: 2.375rem;
   height: 2.375rem;
   background-image: url(${({ url }) => url.src});
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-  transition: all 0.5s;
 `
 
-export const MenuWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 20rem;
-`
+export const MenuList = styled.div``
 
-export const MenuItem = styled.span<{ isSameRoute: boolean; color: string }>`
-  cursor: pointer;
-  ${({ theme }) => theme.typo.text_lg};
-  font-weight: 400;
-  color: ${({ isSameRoute, color, theme }) =>
-    isSameRoute ? color : theme.color.gray[700]};
-  &:hover {
-    color: ${({ theme }) => theme.color.gray[400]};
-  }
-`
+export const MenuItem = styled.span`
 
-export const CreateIcon = styled.div<{ view: string }>`
-  display: ${({ view }) => (view === 'none' ? 'none' : 'flex')};
-`
-
-export const ButtonWrapper = styled.div<{ view: string }>`
-  position: absolute;
-  right: 23%;
-  display: ${({ view }) => view};
-  svg {
-    cursor: pointer;
-    margin-left: 1rem;
-    fill: ${({ theme }) => theme.color.gray[800]};
-    &:hover {
-      fill: ${({ theme }) => theme.color.main};
-    }
-  }
-`
-
-export const LoginButton = styled.div<{ color: string }>`
-  background-color: ${({ color }) => color};
-  padding: 0.5rem 0.75rem;
-  border-radius: 0.5rem;
-  text-align: center;
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  cursor: pointer;
-  backdrop-filter: blur(0.25rem);
-  span {
-    font-size: 1rem;
-    color: ${({ theme }) => theme.color.white};
-    margin: 0;
-  }
-  &:hover {
-    background-color: rgb(209, 209, 209, 0.5);
-  }
-`
-
-export const SelectFilterContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 `
