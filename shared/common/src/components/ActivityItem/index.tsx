b0593b9d362715fa02711a8d802c410027ea5e59
송현우 <shw9052@naver.com>
2023-11-16 23:58:04 +0900

    Add activityItem

--- shared/common/src/components/ActivityItem/index.tsx
+++ shared/common/src/components/ActivityItem/index.tsx
@@ -5,7 +5,21 @@ import * as S from './style'
 const ActivityItem = () => {
   return (
     <S.ActivityItemWrapper>
-
+      <S.TextContainer>
+        <div>
+          <S.AcitivTitle>* 오늘의 일기 *</S.AcitivTitle>
+        </div>
+        <div>
+          <S.MainText>
+            오늘도 사람을 산 채로 갈아서 디자인하기 강의를 들었다.
+          </S.MainText>
+        </div>
+      </S.TextContainer>
+      <div>
+        <S.Number>1점 수여</S.Number>
+        <S.Number>•</S.Number>
+        <S.Number>2023.11.09</S.Number>
+      </div>
     </S.ActivityItemWrapper>
   )
 }
--- shared/common/src/components/ActivityItem/style.ts
+++ shared/common/src/components/ActivityItem/style.ts
@@ -1,5 +1,34 @@
 import styled from '@emotion/styled'
 
 export const ActivityItemWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-content: space-between;
   cursor: pointer;
+  width: 17.625rem;
+  height: 10.5rem;
+  background-color: ${({ theme }) => theme.color.white};
+  border: 0.0625rem solid ${({ theme }) => theme.color.gray[900]};
+  border-radius: 0.5rem;
+  padding: 1rem 1rem;
+  margin: 0.75rem 0.75rem;
+`
+
+export const TextContainer = styled.div`
+  width: 100%;
+`
+
+export const AcitivTitle = styled.span`
+  ${({ theme }) => theme.typo.text_lg};
+`
+
+export const MainText = styled.span`
+  ${({ theme }) => theme.typo.text_md};
+  color: ${({ theme }) => theme.color.gray[400]};
+`
+
+export const Number = styled.span`
+  ${({ theme }) => theme.typo.text_md};
+  color: ${({ theme }) => theme.color.gray[700]};
+  margin-right: 0.5rem;
 `
