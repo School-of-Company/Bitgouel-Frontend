e71f069c99892056fc37d8ce12c47152185925c3
송현우 <shw9052@naver.com>
2023-11-17 16:04:16 +0900

    Update inside activity detail

--- packages/inside/src/PageContainer/Club/ActivityDetail/style.ts
+++ packages/inside/src/PageContainer/Club/ActivityDetail/style.ts
@@ -67,10 +67,37 @@ export const TitleContainer = styled.div`
 
 export const SubTitle = styled.div`
   padding: 0.5rem 0;
+  display: flex;
   span {
     ${({ theme }) => theme.typo.text_md};
     color: ${({ theme }) => theme.color.gray['400']};
   }
+`
+
+export const ApproveStatus = styled.div<{ approveColor: boolean }>`
+  background-color: ${({ theme, approveColor }) =>
+    approveColor ? theme.color.blue['800'] : theme.color.red['800']};
+  color: ${({ theme, approveColor }) =>
+    approveColor ? theme.color.main : theme.color.error};
+  ${({ theme }) => theme.typo.text_md};
+  text-align: center;
+  padding: 0.5rem 1rem;
+  border-radius: 1.125rem;
+  margin-right: 2.5rem;
+`
+
+export const NumberBox = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 2.5rem;
+`
+
+export const SubTitleBox = styled.div`
+  color: ${({ theme }) => theme.color.white};
+  background-color: ${({ theme }) => theme.color.gray['700']};
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  margin-right: 0.5rem;
 `
 
 export const Title = styled.span`
