1ab8fae0e8581f836505b9bc6f71328acd44814d
송현우 <shw9052@naver.com>
2023-11-17 10:40:33 +0900

    Add inside activity create, detail page

---
+++ packages/inside/src/app/main/club/detail/student/activity/detail/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ActivityDetailPage from '@/PageContainer/Club/ActivityDetail'
+
+const ActivityDetail = () => {
+  return <ActivityDetailPage />
+}
+
+export default ActivityDetail
