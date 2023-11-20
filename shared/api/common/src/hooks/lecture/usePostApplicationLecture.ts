4edc0b6c8c07431722077d0deb44b1ca0111893f
TaerangLee <s22051@gsm.hs.kr>
2023-11-20 15:32:12 +0900

    Update change patch -> post

--- shared/api/common/src/hooks/lecture/usePostApplicationLecture.ts
+++ shared/api/common/src/hooks/lecture/usePostApplicationLecture.ts
@@ -1,13 +1,13 @@
 import { useMutation } from '@tanstack/react-query'
 import { lectureQueryKeys } from '../../libs/queryKeys'
 import { lectureUrl } from '../../libs/urlController'
-import { patch } from '../../libs'
+import { post } from '../../libs'
 import { AxiosResponse } from 'axios'
 
 export const usePostApplicationLecture = (id: string) =>
   useMutation<AxiosResponse>(
     lectureQueryKeys.postLectureApplication(id),
-    () => patch(lectureUrl.lectureApplication(id), {}),
+    () => post(lectureUrl.lectureApplication(id), {}),
     {
       onSuccess: (response) => {
         console.log(response)
