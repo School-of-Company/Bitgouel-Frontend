db8da1aee962b7496beab62b9c806d503c7caf58
TaerangLee <s22051@gsm.hs.kr>
2023-11-20 13:26:22 +0900

    Update Query keys naming change

--- shared/api/common/src/libs/queryKeys.ts
+++ shared/api/common/src/libs/queryKeys.ts
@@ -1,58 +1,50 @@
 export const authQueryKeys = {
-  patchReissue: () => ['auth', 'patchReissue'],
-  postLogin: () => ['auth', 'postLogin'],
-  deleteLogout: () => ['auth', 'deleteLogout'],
-  deleteWithDraw: () => ['auth', 'deleteWithDraw'],
-  postSignUpStudent: () => ['auth', 'postSignUpStudent'],
-  postSignUpTeacher: () => ['auth', 'postSignUpTeacher'],
-  postSignUpBbozzak: () => ['auth', 'postSignUpBbozzak'],
-  postSignUpProfessor: () => ['auth', 'postSignUpProfessor'],
-  postSignUpGoverment: () => ['auth', 'postSignUpGoverment'],
-  postSignUpCompanyInstructor: () => ['auth', 'postSignUpCompanyInstructor'],
+  patchReissue: () => ['auth', 'reissue'],
+  postLogin: () => ['auth', 'login'],
+  deleteLogout: () => ['auth', 'logout'],
+  deleteWithDraw: () => ['auth', 'withDraw'],
+  postSignUpStudent: () => ['auth', 'signUp'],
+  postSignUpTeacher: () => ['auth', 'signUp'],
+  postSignUpBbozzak: () => ['auth', 'signUp'],
+  postSignUpProfessor: () => ['auth', 'signUp'],
+  postSignUpGoverment: () => ['auth', 'signUp'],
+  postSignUpCompanyInstructor: () => ['auth', 'signUp'],
 } as const
 
 export const lectureQueryKeys = {
-  postLetureCreate: () => ['lecture', 'postLectureCreat'],
-  getLectureList: () => ['lecture', 'getLectureList'],
-  getLectureDetail: (id: string) => ['lecture', 'getLectureDetail', id],
-  postLectureApplication: (id: string) => [
-    'lecture',
-    'postLectureApplication',
-    id,
-  ],
-  patchLectureApprove: (id: string) => ['lecture', 'patchLectureApprove', id],
-  deleteLectureReject: (id: string) => ['lecture', 'deleteLectureReject', id],
+  postLetureCreate: () => ['lecture', 'creat'],
+  getLectureList: () => ['lecture', 'list'],
+  getLectureDetail: (id: string) => ['lecture', 'detail', id],
+  postLectureApplication: (id: string) => ['lecture', 'application', id],
+  patchLectureApprove: (id: string) => ['lecture', 'approve', id],
+  deleteLectureReject: (id: string) => ['lecture', 'reject', id],
 } as const
 
 export const activityQueryKeys = {
-  postActivityInformation: () => ['activity', 'postActivityInformation'],
+  postActivityInformation: () => ['activity', 'information'],
   patchActivityInformationCorrection: (activity_id: string) => [
     'activity',
-    'patchActivityInformationCorrection',
+    'correction',
     activity_id,
   ],
   patchActivityApprove: (activity_id: string) => [
     'activity',
-    'patchActivityApprove',
+    'approve',
     activity_id,
   ],
   deleteActivityReject: (activity_id: string) => [
     'activity',
-    'deleteActivityReject',
+    'reject',
     activity_id,
   ],
   deleteActivityInformationRemove: (activity_id: string) => [
     'activity',
-    'deleteActivityInformationRemove',
+    'remove',
     activity_id,
   ],
-  getActivityMyselfList: () => ['activity', 'getActivityMyselfList'],
-  getActivityList: (student_id: string) => [
-    'activity',
-    'getActivityList',
-    student_id,
-  ],
-  getActivityInformationList: () => ['activity', 'getActivityInformationList'],
+  getActivityMyselfList: () => ['activity', 'myselfList'],
+  getActivityList: (student_id: string) => ['activity', 'list', student_id],
+  getActivityInformationList: () => ['activity', 'informationList'],
   getActivityInformationDetail: () => (activity_id: string) =>
-    ['activity', 'getActivityInformationDetail', activity_id],
+    ['activity', 'detail', activity_id],
 }
