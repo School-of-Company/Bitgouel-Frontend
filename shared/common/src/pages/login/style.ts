6e9f510e05ba9a9737bc3787e53449a930585806
이운린 <105215297+likegitman@users.noreply.github.com>
2023-11-23 09:05:30 +0900

    Revert "[Common] 로그인 관련 오류 조절"

--- shared/common/src/pages/login/style.ts
+++ shared/common/src/pages/login/style.ts
@@ -89,11 +89,9 @@ export const LoginButtonWrapper = styled.div`
   align-items: flex-end;
 `
 
-export const LoginButton = styled.button<{ isAble: boolean }>`
-  background-color: ${({ theme, isAble }) =>
-    isAble ? theme.color.main : theme.color.gray['700']};
-  color: ${({ theme, isAble }) =>
-    isAble ? theme.color.white : theme.color.gray['400']};
+export const LoginButton = styled.button`
+  background-color: ${({ theme }) => theme.color.main};
+  color: ${({ theme }) => theme.color.white};
   ${({ theme }) => theme.typo.text_lg};
   width: 24rem;
   height: 3.25rem;
