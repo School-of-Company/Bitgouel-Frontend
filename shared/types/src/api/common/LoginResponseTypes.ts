763242a0344a0848acd4ceaff22052df145a3204
unrin <momlin069@gmail.com>
2023-11-27 09:27:13 +0900

    Add autority

--- shared/types/src/api/common/LoginResponseTypes.ts
+++ shared/types/src/api/common/LoginResponseTypes.ts
@@ -1,6 +1,16 @@
+export type RoleEnumTypes =
+  | 'ROLE_ADMIN'
+  | 'ROLE_STUDENT'
+  | 'ROLE_TEACHER'
+  | 'ROLE_BBOZZAK'
+  | 'ROLE_PROFESSOR'
+  | 'ROLE_COMPANY_INSTRUCTOR'
+  | 'ROLE_GOVERNMENT'
+
 export interface LoginResponseTypes {
   accessToken: string
   refreshToken: string
   accessExpiredAt: string
   refreshExpiredAt: string
+  authority: RoleEnumTypes
 }
