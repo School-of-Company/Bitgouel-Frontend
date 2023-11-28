83acf47ea9ba8c561417039ec9d0dc167aa99fb5
송현우 <shw9052@naver.com>
2023-11-28 15:20:43 +0900

    Add club intro item

--- shared/common/src/pages/home/style.ts
+++ shared/common/src/pages/home/style.ts
@@ -187,4 +187,48 @@ export const SchoolItemContainer = styled.div`
 
 export const ClubListContainer = styled.div`
   margin-top: 10rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const ClubIntroListWrapper = styled.div`
+  width: 75rem;
+  display: flex;
+  justify-content: space-between;
+  &:last-child {
+    margin-top: 4rem;
+  }
+`
+
+export const ClubIntroList = styled.div`
+  margin-top: 5rem;
+`
+
+export const ClubIntro = styled.div`
+  width: 24rem;
+`
+
+export const ClubIntroSubTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 24px;
+`
+
+export const ClubIntroTitle = styled.span`
+  color: ${({ theme }) => theme.color.black};
+  ${({ theme }) => theme.typo.text_md};
+`
+
+export const ClubIntroSubTitle = styled.span`
+  color: ${({ theme }) => theme.color.gray[400]};
+  ${({ theme }) => theme.typo.text_md};
+`
+
+export const ClubIntroText = styled.div`
+  margin: 16px 24px 0px;
+  li {
+    color: ${({ theme }) => theme.color.gray[700]};
+    ${({ theme }) => theme.typo.text_sm};
+  }
 `
