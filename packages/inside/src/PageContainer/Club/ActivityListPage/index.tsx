c65a81a297a2e0dcca60de4742fd2205189a455e
TaerangLee <s22051@gsm.hs.kr>
2023-12-05 10:48:01 +0900

    Update activity list page

--- packages/inside/src/PageContainer/Club/ActivityListPage/index.tsx
+++ packages/inside/src/PageContainer/Club/ActivityListPage/index.tsx
@@ -7,49 +7,28 @@ import { useRouter } from 'next/navigation'
 import { ActivityItem } from '@bitgouel/common/src/components'
 import { ApproveStatusEnum } from '@bitgouel/types'
 
-// 아래 예시인 activityList의 타입인데 어차피 통신으로 값을
-// 갖고 올거니 지워주세요 작업할 때
-interface ActivityItemType {
-  activityId: string
-  title: string
-  userId: string
-  activityDate: string
-  userName: string
-  approveStatus: ApproveStatusEnum
-}
+import {
+  useGetActivityInformationList,
+  useGetActivityMyselfList,
+} from '@bitgouel/api'
 
 const ActivityListPage = () => {
   const router = useRouter()
 
-  const activityList: ActivityItemType[] = [
-    {
-      activityId: '1',
-      title:
-        '국가는 국민 모두의 생산 및 생활의 기반이 되는 국토의 효율적이고 균형있는 이용·개발과 보전을 위하여 법률이 정하는 바에 의하여 그에 관한 필요한 제한과 의무를 과할 수 있다.',
-      userId: 'sdfsf',
-      activityDate: '2023-12-13',
-      userName: '박주홍',
-      approveStatus: 'APPROVED',
-    },
-    {
-      activityId: '2',
-      title:
-        '국가는 국민 모두의 생산 및 생활의 기반이 되는 국토의 효율적이고 균형있는 이용·개발과 보전을 위하여 법률이 정하는 바에 의하여 그에 관한 필요한 제한과 의무를 과할 수 있다.',
-      userId: 'sdfsf',
-      activityDate: '2023-12-13',
-      userName: '박주홍',
-      approveStatus: 'APPROVED',
-    },
-    {
-      activityId: '3',
-      title:
-        '국가는 국민 모두의 생산 및 생활의 기반이 되는 국토의 효율적이고 균형있는 이용·개발과 보전을 위하여 법률이 정하는 바에 의하여 그에 관한 필요한 제한과 의무를 과할 수 있다.',
-      userId: 'sdfsf',
-      activityDate: '2023-12-13',
-      userName: '박주홍',
-      approveStatus: 'APPROVED',
-    },
-  ]
+  const role = 'student'
+
+  const { data } = useGetActivityMyselfList({
+    page: 1,
+    size: 5,
+  }) //학생 조회
+
+  const { data: adminData } = useGetActivityInformationList({
+    page: 1,
+    size: 4,
+  }) // admin 조회
+
+  console.log(data)
+  console.log(adminData)
 
   return (
     <div>
@@ -69,9 +48,13 @@ const ActivityListPage = () => {
 
       <S.ActivityWrapper>
         <S.ActivityContainer>
-          {activityList.map((activity) => (
-            <ActivityItem item={activity} key={activity.activityId} />
-          ))}
+          {role === 'student'
+            ? data?.data.activities.content.map((activity) => (
+                <ActivityItem item={activity} key={activity.activityId} />
+              ))
+            : adminData?.data.activities.content.map((activity) => (
+                <ActivityItem item={activity} key={activity.activityId} />
+              ))}
         </S.ActivityContainer>
       </S.ActivityWrapper>
     </div>
