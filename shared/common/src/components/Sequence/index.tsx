789e103f6bde47761f9470c9feffcb6cb4c8c96d
송현우 <shw9052@naver.com>
2023-12-05 11:35:13 +0900

    Update sequence map

--- shared/common/src/components/Sequence/index.tsx
+++ shared/common/src/components/Sequence/index.tsx
@@ -2,24 +2,31 @@
 import * as S from './style'
 import { Circle } from '../../assets/index'
 
+const sequenceList = [
+  { name: '사업소개', scroll: 722 },
+  { name: '관련 학교 소개', scroll: 1580 },
+  { name: '취업 동아리 소개', scroll: 2500 },
+  { name: '연계 대학 소개', scroll: 4000 },
+  { name: '참여 기업 소개', scroll: 4900 },
+  { name: '유관 기관 소개', scroll: 5800 },
+  { name: '자주 묻는 질문', scroll: 6700 },
+]
+
 const Sequence = () => {
-  const sequenceList = [
-    '사업소개',
-    '관련 학교 소개',
-    '연계 대학 소개',
-    '관련 기관 소개',
-    '취업 동아리 소개',
-    '참여 기업 소개',
-    '자주 묻는 질문',
-  ]
-
-
   return (
     <S.SequenceWrapper>
       {sequenceList.map((sequence, idx) => (
-        <S.TextContainer key={idx}>
+        <S.TextContainer
+          onClick={() => {
+            window.scrollTo({
+              top: sequence.scroll,
+              behavior: 'smooth',
+            })
+          }}
+          key={idx}
+        >
           <Circle />
-          {sequence}
+          {sequence.name}
           <div />
         </S.TextContainer>
       ))}
