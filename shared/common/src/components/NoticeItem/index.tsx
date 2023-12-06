236ca6a73c0ca952f9b53309158e1ffb930c11a9
송현우 <shw9052@naver.com>
2023-12-06 20:05:15 +0900

    Add notice item

--- shared/common/src/components/NoticeItem/index.tsx
+++ shared/common/src/components/NoticeItem/index.tsx
@@ -3,6 +3,15 @@
 import * as S from './style'
 
 const NoticeItem = () => {
-  return <S.NoticeItem></S.NoticeItem>
+  return (
+    <S.NoticeItem>
+      <div>
+        <S.NoticeTitle>마이다스 IT 기업탐방</S.NoticeTitle>
+      </div>
+      <div>
+        <S.NoticeDate>2023년 11월 11일</S.NoticeDate>
+      </div>
+    </S.NoticeItem>
+  )
 }
 export default NoticeItem
--- shared/common/src/components/NoticeItem/style.ts
+++ shared/common/src/components/NoticeItem/style.ts
@@ -1,3 +1,23 @@
 import styled from '@emotion/styled'
 
-export const NoticeItem = styled.div``
+export const NoticeItem = styled.div`
+  width: 100%;
+  height: 3.625rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 0.0625rem solid ${({ theme }) => theme.color.gray['900']};
+  div {
+    margin: 0 1rem;
+  }
+`
+
+export const NoticeTitle = styled.span`
+  ${({ theme }) => theme.typo.text_lg.medium};
+  color: ${({ theme }) => theme.color.black};
+`
+
+export const NoticeDate = styled.span`
+  ${({ theme }) => theme.typo.text_md.regular};
+  color: ${({ theme }) => theme.color.gray['400']};
+`
