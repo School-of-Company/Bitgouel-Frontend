42bcd78670655d51c45a5845e39469f363da94f9
송현우 <shw9052@naver.com>
2023-12-06 21:02:11 +0900

    Add notice detail page

--- shared/common/src/pages/notice/detail/style.ts
+++ shared/common/src/pages/notice/detail/style.ts
@@ -22,3 +22,65 @@ export const ClubTitle = styled.span`
   color: ${({ theme }) => theme.color.white};
   ${({ theme }) => theme.typo.title_lg.semibold};
 `
+
+export const DocumentWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`
+
+export const Document = styled.div`
+  width: 75rem;
+`
+
+export const TitleContainer = styled.div`
+  margin-top: 2rem;
+`
+
+export const Title = styled.span`
+  ${({ theme }) => theme.typo.title_md.semibold};
+  color: ${({ theme }) => theme.color.black};
+`
+
+export const SubTitle = styled.div`
+  padding: 0.5rem 0;
+  display: flex;
+  span {
+    ${({ theme }) => theme.typo.text_md.regular};
+    color: ${({ theme }) => theme.color.gray['400']};
+  }
+`
+
+export const ApproveStatus = styled.div<{ approveColor: boolean }>`
+  background-color: ${({ theme, approveColor }) =>
+    approveColor ? theme.color.blue['800'] : theme.color.red['800']};
+  color: ${({ theme, approveColor }) =>
+    approveColor ? theme.color.main : theme.color.error};
+  ${({ theme }) => theme.typo.text_md.regular};
+  text-align: center;
+  padding: 0.5rem 1rem;
+  border-radius: 1.125rem;
+  margin-right: 2.5rem;
+`
+
+export const NumberBox = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 2.5rem;
+`
+
+export const SubTitleBox = styled.div`
+  color: ${({ theme }) => theme.color.white};
+  background-color: ${({ theme }) => theme.color.gray['700']};
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  margin-right: 0.5rem;
+`
+
+export const MainText = styled.div`
+  color: ${({ theme }) => theme.color.gray['400']};
+  ${({ theme }) => theme.typo.text_sm.regular};
+  line-height: 1.5rem;
+  margin-top: 2.25rem;
+  padding-bottom: 6.25rem;
+`
