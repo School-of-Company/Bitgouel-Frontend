d96bdaa736b419e64f333876f8665ca5cce58094
송현우 <shw9052@naver.com>
2023-12-06 18:27:48 +0900

    Add notice item

---
+++ shared/common/src/components/NoticeItem/index.stories.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import NoticeItem from '.'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'common/components/NoticeItem',
+  component: NoticeItem,
+} as Meta<typeof NoticeItem>
+
+type Story = StoryObj<typeof NoticeItem>
+
+export const Primary: Story = {}
---
+++ shared/common/src/components/NoticeItem/index.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import * as S from './style'
+
+const NoticeItem = () => {
+  return <S.NoticeItem></S.NoticeItem>
+}
+export default NoticeItem
