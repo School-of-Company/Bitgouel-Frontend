bc9bf29b36c8511c70d7062749a50f479cede151
TaerangLee <s22051@gsm.hs.kr>
2023-12-06 13:49:04 +0900

    Update activity modify page

--- packages/inside/src/PageContainer/Club/ActivityModifyPage/index.tsx
+++ packages/inside/src/PageContainer/Club/ActivityModifyPage/index.tsx
@@ -3,20 +3,30 @@
 import * as S from './style'
 import Bg2 from '@bitgouel/common/src/assets/png/mainBg2.png'
 import { Chevron } from '@bitgouel/common'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { SelectCalendarModal, SelectScoreModal } from '@bitgouel/common'
 import { useSearchParams } from 'next/navigation'
+import { useGetActivityDetail, usePatchActivityCorrection } from '@bitgouel/api'
+import { ActivityDetailTypes } from '@bitgouel/types'
 
-const ActivityModifyPage = () => {
+const ActivityModifyPage = ({ activity_Id }: { activity_Id: string }) => {
   const MAXLENGTH: number = 1000 as const
+
+  const { data } = useGetActivityDetail(activity_Id)
+  const { mutate } = usePatchActivityCorrection(activity_Id)
 
   const [isActivityDate, setIsActivityDate] = useState<boolean>(false)
   const [activityDate, setActivityDate] = useState<Date>(new Date())
-  const [activityDateText, setActivityDateText] =
-    useState<string>('활동 날짜 선택')
+  const [activityDateText, setActivityDateText] = useState<string | any>(
+    data?.data.activityDate
+  )
 
   const [isScore, setIsScore] = useState<boolean>(false)
-  const [scoreText, setScoreText] = useState<string>('학점 선택')
+  const [scoreText, setScoreText] = useState<string | any>(data?.data.credit)
+  const [modifyData, setModifyData] = useState<ActivityDetailTypes | any>(data)
+
+  console.log(modifyData)
+  console.log(modifyData)
 
   const openSelectModal = (type: string) => {
     if (type === '학점 선택') {
@@ -31,6 +41,22 @@ const ActivityModifyPage = () => {
   const params = useSearchParams()
   console.log(params.get('title'))
 
+  const onModifyData = () => {
+    mutate({
+      title: modifyData.title,
+      content: modifyData.content,
+      credit: +scoreText.slice(0, 1),
+      activityDate: `${activityDate.getFullYear()}-${(
+        activityDate.getMonth() + 1
+      )
+        .toString()
+        .padStart(2, '0')}-${activityDate
+        .getDate()
+        .toString()
+        .padStart(2, '0')}`,
+    })
+  }
+
   return (
     <div>
       <S.SlideBg url={Bg2}>
@@ -41,10 +67,24 @@ const ActivityModifyPage = () => {
       </S.SlideBg>
       <S.DocumentInputContainer>
         <S.DocumentInput>
-          <S.InputTitle placeholder='활동 제목(100자 이내)' maxLength={100} />
+          <S.InputTitle
+            placeholder='활동 제목(100자 이내)'
+            maxLength={100}
+            value={data?.data.title}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              setModifyData((prev) => ({ ...prev, title: event.target.value }))
+            }
+          />
           <S.InputMainText
             maxLength={MAXLENGTH}
             placeholder='활동 내용 작성 (1000자 이내)'
+            value={data?.data.content}
+            onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+              setModifyData((prev) => ({
+                ...prev,
+                content: event.target.value,
+              }))
+            }
           />
           <S.ActivitySetting>
             <S.SettingTitle>활동 세부 설정</S.SettingTitle>
@@ -84,7 +124,9 @@ const ActivityModifyPage = () => {
             </S.SettingSelectionContainer>
           </S.ActivitySetting>
           <S.ButtonContainer>
-            <S.CreateButton>활동 수정하기</S.CreateButton>
+            <S.CreateButton onClick={onModifyData}>
+              활동 수정하기
+            </S.CreateButton>
           </S.ButtonContainer>
         </S.DocumentInput>
       </S.DocumentInputContainer>
