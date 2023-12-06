a8a6dc7aafb850317a6a53b7495d95099f84c5e0
TaerangLee <s22051@gsm.hs.kr>
2023-12-06 20:49:07 +0900

    Update usePostActivityInformation success alert appoint

--- shared/api/inside/src/hooks/activity/usePostActivityInformation.ts
+++ shared/api/inside/src/hooks/activity/usePostActivityInformation.ts
@@ -1,18 +1,28 @@
 import { useMutation } from '@tanstack/react-query'
 import { activityQueryKeys, post, activityUrl } from '../../../../common'
 import { AxiosResponse } from 'axios'
+import { useRouter } from 'next/navigation'
 import { ActivityPayloadTypes } from '@bitgouel/types'
+import { toast } from 'react-toastify'
+import { useModal } from '@bitgouel/common/src/hooks'
 
-export const usePostActivityInformation = () =>
-  useMutation<AxiosResponse, Error, ActivityPayloadTypes>(
+export const usePostActivityInformation = () => {
+  const router = useRouter()
+  const { closeModal } = useModal()
+
+  return useMutation<AxiosResponse, Error, ActivityPayloadTypes>(
     activityQueryKeys.postActivityInformation(),
     () => post(activityUrl.activityInformation(), {}),
     {
-      onSuccess: ({ data }) => {
-        console.log(data)
+      onSuccess: () => {
+        closeModal()
+        router.push('/main/club/student/activity')
+        toast.success('개설 신청이 완료되었습니다')
+        toast.info('관리자의 승인을 기다려주세요')
       },
       onError: (error) => {
         console.log(error)
       },
     }
   )
+}
