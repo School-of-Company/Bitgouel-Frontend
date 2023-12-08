df605510b60bb1a3c1ab7ec00901c882f83cc7b5
TaerangLee <s22051@gsm.hs.kr>
2023-12-08 21:10:24 +0900

    Update modal type

--- shared/types/src/common/SelectScoreModalProps.ts
+++ shared/types/src/common/SelectScoreModalProps.ts
@@ -1,5 +1,5 @@
 export interface SelectScoreModalProps {
-  score: string
+  score?: string
   setScore: React.Dispatch<React.SetStateAction<string>>
   setIsScore: React.Dispatch<React.SetStateAction<boolean>>
 }
