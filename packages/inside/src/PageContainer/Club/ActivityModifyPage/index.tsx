28e6572831971ab4e4697f7438d406bd82c530e7
TaerangLee <s22051@gsm.hs.kr>
2023-12-10 23:17:26 +0900

    Update modify page title, content exception handling

--- packages/inside/src/PageContainer/Club/ActivityModifyPage/index.tsx
+++ packages/inside/src/PageContainer/Club/ActivityModifyPage/index.tsx
@@ -3,7 +3,7 @@
 import * as S from './style'
 import Bg2 from '@bitgouel/common/src/assets/png/mainBg2.png'
 import { Chevron, CreateModal, useModal } from '@bitgouel/common'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { SelectCalendarModal, SelectScoreModal } from '@bitgouel/common'
 import { useSearchParams } from 'next/navigation'
 import { useGetActivityDetail, usePatchActivityCorrection } from '@bitgouel/api'
@@ -17,6 +17,8 @@ const ActivityModifyPage = ({ activityId }: { activityId: string }) => {
   const { mutate } = usePatchActivityCorrection(activityId)
 
   const [modifyData, setModifyData] = useState<ActivityDetailTypes>(data?.data)
+
+  console.log(modifyData)
 
   const [isActivityDate, setIsActivityDate] = useState<boolean>(false)
   const [activityDate, setActivityDate] = useState<Date>(new Date())
@@ -43,107 +45,119 @@ const ActivityModifyPage = ({ activityId }: { activityId: string }) => {
   console.log(params.get('title'))
 
   const onModifyData = () => {
-    mutate({
-      title: modifyData.title,
-      content: modifyData.content,
-      credit: Number(scoreText?.slice(0, 1)),
-      activityDate: `${activityDate.getFullYear()}-${(
-        activityDate.getMonth() + 1
-      )
-        .toString()
-        .padStart(2, '0')}-${activityDate
-        .getDate()
-        .toString()
-        .padStart(2, '0')}`,
-    })
+    if (modifyData)
+      mutate({
+        title: modifyData.title,
+        content: modifyData.content,
+        credit: Number(scoreText?.slice(0, 1)),
+        activityDate: `${activityDate.getFullYear()}-${(
+          activityDate.getMonth() + 1
+        )
+          .toString()
+          .padStart(2, '0')}-${activityDate
+          .getDate()
+          .toString()
+          .padStart(2, '0')}`,
+      })
     console.log(scoreText)
   }
 
+  useEffect(() => {
+    setModifyData(data?.data)
+  }, [data])
+
   return (
-    <div>
-      <S.SlideBg url={Bg2}>
-        <S.BgContainer>
-          <S.ModifyTitle>활동 수정</S.ModifyTitle>
-          <S.ButtonContainer></S.ButtonContainer>
-        </S.BgContainer>
-      </S.SlideBg>
-      <S.DocumentInputContainer>
-        <S.DocumentInput>
-          <S.InputTitle
-            placeholder='활동 제목(100자 이내)'
-            maxLength={100}
-            value={data?.data.title}
-            onChange={(event: ChangeEvent<HTMLInputElement>) =>
-              setModifyData((prev) => ({ ...prev, title: event.target.value }))
-            }
-          />
-          <S.InputMainText
-            maxLength={MAXLENGTH}
-            placeholder='활동 내용 작성 (1000자 이내)'
-            value={data?.data.content}
-            onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
-              setModifyData((prev) => ({
-                ...prev,
-                content: event.target.value,
-              }))
-            }
-          />
-          <S.ActivitySetting>
-            <S.SettingTitle>활동 세부 설정</S.SettingTitle>
-            <S.SettingSelectionContainer>
-              <S.SettingSelection>
-                <S.SelectModalContainer>
-                  {isActivityDate && (
-                    <SelectCalendarModal
-                      date={activityDate}
-                      setDate={setActivityDate}
-                      setText={setActivityDateText}
-                    />
-                  )}
-                  <S.SettingDateBox onClick={() => openSelectModal('')}>
-                    <Chevron />
-                    <S.SettingButton>{activityDateText}</S.SettingButton>
-                  </S.SettingDateBox>
-                </S.SelectModalContainer>
-              </S.SettingSelection>
-              <S.SettingSelection>
-                <S.SelectModalContainer>
-                  {isScore && (
-                    <SelectScoreModal
-                      score={scoreText}
-                      setScore={setScoreText}
-                      setIsScore={setIsScore}
-                    />
-                  )}
-                  <S.SettingScoreBox
-                    onClick={() => openSelectModal('학점 선택')}
-                  >
-                    <Chevron />
-                    <S.SettingButton>{scoreText + '점'}</S.SettingButton>
-                  </S.SettingScoreBox>
-                </S.SelectModalContainer>
-              </S.SettingSelection>
-            </S.SettingSelectionContainer>
-          </S.ActivitySetting>
-          <S.ButtonContainer>
-            <S.CreateButton
-              onClick={() =>
-                openModal(
-                  <CreateModal
-                    question='활동을 수정하시겠습니까?'
-                    title={data?.data.title}
-                    onCreate={onModifyData}
-                    createText='수정하기'
-                  />
-                )
-              }
-            >
-              활동 수정하기
-            </S.CreateButton>
-          </S.ButtonContainer>
-        </S.DocumentInput>
-      </S.DocumentInputContainer>
-    </div>
+    <>
+      {modifyData && (
+        <div>
+          <S.SlideBg url={Bg2}>
+            <S.BgContainer>
+              <S.ModifyTitle>활동 수정</S.ModifyTitle>
+              <S.ButtonContainer></S.ButtonContainer>
+            </S.BgContainer>
+          </S.SlideBg>
+          <S.DocumentInputContainer>
+            <S.DocumentInput>
+              <S.InputTitle
+                placeholder='활동 제목(100자 이내)'
+                maxLength={100}
+                value={modifyData.title}
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  setModifyData((prev) => ({
+                    ...prev,
+                    title: event.target.value,
+                  }))
+                }
+              />
+              <S.InputMainText
+                maxLength={MAXLENGTH}
+                placeholder='활동 내용 작성 (1000자 이내)'
+                value={modifyData.content}
+                onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+                  setModifyData((prev) => ({
+                    ...prev,
+                    content: event.target.value,
+                  }))
+                }
+              />
+              <S.ActivitySetting>
+                <S.SettingTitle>활동 세부 설정</S.SettingTitle>
+                <S.SettingSelectionContainer>
+                  <S.SettingSelection>
+                    <S.SelectModalContainer>
+                      {isActivityDate && (
+                        <SelectCalendarModal
+                          date={activityDate}
+                          setDate={setActivityDate}
+                          setText={setActivityDateText}
+                        />
+                      )}
+                      <S.SettingDateBox onClick={() => openSelectModal('')}>
+                        <Chevron />
+                        <S.SettingButton>{activityDateText}</S.SettingButton>
+                      </S.SettingDateBox>
+                    </S.SelectModalContainer>
+                  </S.SettingSelection>
+                  <S.SettingSelection>
+                    <S.SelectModalContainer>
+                      {isScore && (
+                        <SelectScoreModal
+                          score={scoreText}
+                          setScore={setScoreText}
+                          setIsScore={setIsScore}
+                        />
+                      )}
+                      <S.SettingScoreBox
+                        onClick={() => openSelectModal('학점 선택')}
+                      >
+                        <Chevron />
+                        <S.SettingButton>{scoreText + '점'}</S.SettingButton>
+                      </S.SettingScoreBox>
+                    </S.SelectModalContainer>
+                  </S.SettingSelection>
+                </S.SettingSelectionContainer>
+              </S.ActivitySetting>
+              <S.ButtonContainer>
+                <S.CreateButton
+                  onClick={() =>
+                    openModal(
+                      <CreateModal
+                        question='활동을 수정하시겠습니까?'
+                        title={data?.data.title}
+                        onCreate={onModifyData}
+                        createText='수정하기'
+                      />
+                    )
+                  }
+                >
+                  활동 수정하기
+                </S.CreateButton>
+              </S.ButtonContainer>
+            </S.DocumentInput>
+          </S.DocumentInputContainer>
+        </div>
+      )}
+    </>
   )
 }
 
