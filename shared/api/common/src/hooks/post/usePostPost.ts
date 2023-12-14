9876390f20529771597f744cea3aa3d07ae1e3bb
unrin <momlin069@gmail.com>
2023-12-14 18:16:44 +0900

    Update usePostPost onSuccess

--- shared/api/common/src/hooks/post/usePostPost.ts
+++ shared/api/common/src/hooks/post/usePostPost.ts
@@ -4,17 +4,22 @@ import { AxiosError } from 'axios'
 import { PostCreatePayloadTypes } from '@bitgouel/types'
 import { useRouter } from 'next/navigation'
 import { toast } from 'react-toastify'
+import { useModal } from '@bitgouel/common/src/hooks'
 
 export const usePostPost = (type: '게시' | '공지') => {
   const { push } = useRouter()
+  const { closeModal } = useModal()
 
   return useMutation<void, AxiosError, PostCreatePayloadTypes>(
     postQueryKeys.postBoardCreate(),
     (createValues) => post(postUrl.postCreate(), createValues),
     {
       onSuccess: () => {
+        closeModal()
         push('/main/post')
-        toast.success(`${type}글을 추가했습니다`)
+        toast.success(
+          `${type === '공지' ? '공지사항' : '게시글'}을 추가했습니다`
+        )
       },
       onError: ({ response }) => {
         console.log(response)
