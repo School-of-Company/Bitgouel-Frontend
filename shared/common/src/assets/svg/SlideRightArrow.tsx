44ae29216d18b5db851bc4f92285d2a0a63150e0
이운린 <105215297+likegitman@users.noreply.github.com>
2023-12-26 13:57:14 +0900

    Update svg px to rem

--- shared/common/src/assets/svg/SlideRightArrow.tsx
+++ shared/common/src/assets/svg/SlideRightArrow.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const SlideRightArrow = () => {
   return (
     <svg
-      width='22'
-      height='40'
+      width='1.375rem'
+      height='2.5rem'
       viewBox='0 0 22 40'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
