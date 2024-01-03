dd67eb6064dbb47e062785ad082736c7d980aad9
송현우 <shw9052@naver.com>
2024-01-03 15:51:07 +0900

    Delete school list oveflow hidden

--- shared/common/src/pages/home/style.ts
+++ shared/common/src/pages/home/style.ts
@@ -208,7 +208,6 @@ export const SemiTitleBox = styled.div`
 export const SchoolIntroWrapper = styled.div`
   width: 100vw;
   height: 35rem;
-  overflow: hidden;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -220,7 +219,6 @@ export const SchoolIntroListContainer = styled.div`
   position: relative;
   width: 100vw;
   height: 15rem;
-  overflow: hidden;
 `
 
 const SchoolIntroListExample = styled.div`
