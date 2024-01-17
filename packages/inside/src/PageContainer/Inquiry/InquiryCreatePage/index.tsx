4d25d5c91d72f796526f8da0e9c0d91cfd03f466
송현우 <shw9052@naver.com>
2024-01-17 17:05:24 +0900

    Update inquiry create btn color

--- packages/inside/src/PageContainer/Inquiry/InquiryCreatePage/index.tsx
+++ packages/inside/src/PageContainer/Inquiry/InquiryCreatePage/index.tsx
@@ -1,10 +1,24 @@
 'use client'
 
+import { useState } from 'react'
 import * as S from './style'
 import { Bg5 } from '@bitgouel/common'
 
 const InquiryCreatePage = () => {
   const MAXLENGTH: number = 1000 as const
+
+  const [inquiryTitle, setInquiryTitle] = useState<string>('')
+  const [inquiryContent, setInquiryContent] = useState<string>('')
+
+  const saveInquiryTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInquiryTitle(event.target.value)
+  }
+
+  const saveInquiryMainText = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setInquiryContent(event.target.value)
+  }
 
   return (
     <div>
@@ -15,13 +29,24 @@ const InquiryCreatePage = () => {
       </S.SlideBg>
       <S.DocumentInputContainer>
         <S.DocumentInput>
-          <S.InputTitle placeholder='문의 제목(100자 이내)' maxLength={100} />
+          <S.InputTitle
+            placeholder='문의 제목(100자 이내)'
+            maxLength={100}
+            onChange={saveInquiryTitle}
+          />
           <S.InputMainText
             maxLength={MAXLENGTH}
             placeholder='본문 입력 (1000자 이내)'
+            onChange={saveInquiryMainText}
           />
           <S.ButtonContainer>
-            <S.CreateButton>문의하기</S.CreateButton>
+            <S.CreateButton
+              isAble={
+                inquiryTitle !== '' && inquiryContent !== '' ? true : false
+              }
+            >
+              문의하기
+            </S.CreateButton>
           </S.ButtonContainer>
         </S.DocumentInput>
       </S.DocumentInputContainer>
