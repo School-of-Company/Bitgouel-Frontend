ab2d792baaab0f4bcacb5df3c22779e6bf2eb204
TaerangLee <s22051@gsm.hs.kr>
2024-01-29 22:40:52 +0900

    Update modify api array -> object, id optional

--- shared/api/inside/src/hooks/certificate/usePatchModifyCertificate.ts
+++ shared/api/inside/src/hooks/certificate/usePatchModifyCertificate.ts
@@ -3,10 +3,10 @@ import { useMutation } from '@tanstack/react-query'
 import { AxiosError } from 'axios'
 import { certificateQueryKeys, certificateUrl, patch } from '../../../../common'
 
-export const usePatchModifyCertificate = (student_id: string, id: string) => {
-  return useMutation<void, AxiosError, Certificate[]>(
-    certificateQueryKeys.patchCertificateModify(student_id, id),
+export const usePatchModifyCertificate = (studentId: string, id: string) => {
+  return useMutation<void, AxiosError, Certificate>(
+    certificateQueryKeys.patchCertificateModify(studentId, id),
     (modifyValue) =>
-      patch(certificateUrl.certificateModify(student_id, id), modifyValue)
+      patch(certificateUrl.certificateModify(studentId, id), modifyValue)
   )
 }
--- shared/types/src/api/client/CertificateTypes.ts
+++ shared/types/src/api/client/CertificateTypes.ts
@@ -1,5 +1,5 @@
 export interface Certificate {
-  id: string
+  id?: string
   name: string
   acquisitionDate: string
 }
