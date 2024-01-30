881c10f7fd89f96cd079b7572c773c6d89efa369
TaerangLee <s22051@gsm.hs.kr>
2024-01-30 17:39:14 +0900

    Delete studentId in modifyApi

--- shared/api/inside/src/hooks/certificate/usePatchModifyCertificate.ts
+++ shared/api/inside/src/hooks/certificate/usePatchModifyCertificate.ts
@@ -3,10 +3,9 @@ import { useMutation } from '@tanstack/react-query'
 import { AxiosError } from 'axios'
 import { certificateQueryKeys, certificateUrl, patch } from '../../../../common'
 
-export const usePatchModifyCertificate = (studentId: string, id: string) => {
+export const usePatchModifyCertificate = (id: string) => {
   return useMutation<void, AxiosError, Certificate>(
-    certificateQueryKeys.patchCertificateModify(studentId, id),
-    (modifyValue) =>
-      patch(certificateUrl.certificateModify(studentId, id), modifyValue)
+    certificateQueryKeys.patchCertificateModify(id),
+    (modifyValue) => patch(certificateUrl.certificateModify(id), modifyValue)
   )
 }
