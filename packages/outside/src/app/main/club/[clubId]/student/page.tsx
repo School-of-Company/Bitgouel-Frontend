613eed295088566723be6ca72c69e1b72289042f
TaerangLee <s22051@gsm.hs.kr>
2024-01-31 21:36:39 +0900

    Update outside studentPage id

--- packages/outside/src/app/main/club/[clubId]/student/page.tsx
+++ packages/outside/src/app/main/club/[clubId]/student/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { StudentPage } from '@bitgouel/common/src/pages'
 
-const Student = () => {
-  return <StudentPage />
+const Student = ({ params }: { params: { studentId; clubId: string } }) => {
+  return <StudentPage studentId={params.studentId} clubId={params.clubId} />
 }
 
 export default Student
