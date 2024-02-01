156ff58ae6ad7f49a9b3eb74f81092eab4d6bcd4
TaerangLee <s22051@gsm.hs.kr>
2024-02-01 17:46:02 +0900

    Fix studentId type appoint

--- packages/inside/src/app/main/club/[clubId]/student/[studentId]/page.tsx
+++ packages/inside/src/app/main/club/[clubId]/student/[studentId]/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { StudentPage } from '@bitgouel/common/src/pages'
 
-const Student = ({ params }: { params: { studentId; clubId: string } }) => {
+const Student = ({
+  params,
+}: {
+  params: { studentId: string; clubId: string }
+}) => {
   return <StudentPage studentId={params.studentId} clubId={params.clubId} />
 }
 
