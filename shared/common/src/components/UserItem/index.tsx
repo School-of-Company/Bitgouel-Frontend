2b6d603060b503ef0d3ee4d6d3e23861eb60b1d2
송현우 <shw9052@naver.com>
2024-02-08 19:24:18 +0900

    Add userItem component

---
+++ shared/common/src/components/UserItem/index.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import * as S from './style'
+
+const UserItem = () => {
+  return <S.UserItem>
+    <div>
+      <S.Name>홍길동</S.Name>
+      <S.Role>학생</S.Role>
+    </div>
+  </S.UserItem>
+}
+
+export default UserItem
