0b4d47884bcd26855142db1ac4ebadb6206a7ddc
송현우 <shw9052@naver.com>
2024-02-08 19:23:48 +0900

    Add filter out svg

---
+++ shared/common/src/assets/svg/FilterOut.tsx
@@ -0,0 +1,17 @@
+const FilterOut = () => {
+  return (
+    <svg
+      width='20'
+      height='14'
+      viewBox='0 0 20 14'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect width='20' height='2' rx='1' fill='#6B6B6B' />
+      <rect x='3' y='6' width='14' height='2' rx='1' fill='#6B6B6B' />
+      <rect x='6' y='12' width='8' height='2' rx='1' fill='#6B6B6B' />
+    </svg>
+  )
+}
+
+export default FilterOut
