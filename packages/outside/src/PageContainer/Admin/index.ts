c26323475516e35772047878e66e431ca6f34b2b
송현우 <shw9052@naver.com>
2024-02-09 20:05:35 +0900

    Add quitUserPage

--- packages/outside/src/PageContainer/Admin/index.ts
+++ packages/outside/src/PageContainer/Admin/index.ts
@@ -1,2 +1,3 @@
 export { default as UserListPage } from './UserListPage'
 export { default as NewUserListPage } from './NewUserListPage'
+export { default as QuitUserListPage } from './QuitUserListPage'
---
+++ packages/outside/src/app/main/admin/quit/page.tsx
@@ -0,0 +1,8 @@
+import { QuitUserListPage } from '@/PageContainer'
+import React from 'react'
+
+const QuitUserList = () => {
+  return <QuitUserListPage />
+}
+
+export default QuitUserList
