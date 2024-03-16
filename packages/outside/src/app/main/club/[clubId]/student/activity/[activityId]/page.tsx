2707c9cfa4ee980c51418ad3e84806a5df1a2e02
TaerangLee <s22051@gsm.hs.kr>
2024-03-16 17:44:05 +0900

    Delete unless the outside files

--- packages/outside/src/app/main/club/[clubId]/student/activity/[activityId]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ActivityDetailPage } from '@/PageContainer'
-
-const ActivityDetail = ({ params }: { params: { activityId: string } }) => {
-  return <ActivityDetailPage activityId={params.activityId} />
-}
-
-export default ActivityDetail
