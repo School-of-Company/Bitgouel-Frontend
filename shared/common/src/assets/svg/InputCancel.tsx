38765ccf657b7f82231ba7c07f3414a2d21ca9da
이운린 <105215297+likegitman@users.noreply.github.com>
2024-03-17 01:05:08 +0900

    Update svg unit

--- shared/common/src/assets/svg/InputCancel.tsx
+++ shared/common/src/assets/svg/InputCancel.tsx
@@ -3,9 +3,9 @@ import { SVGProps } from 'react'
 const InputCancel = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg
-      width='20'
-      height='20'
-      viewBox='0 0 20 20'
+      width='1.25rem'
+      height='1.25rem'
+      viewBox='0 0 1.25rem 1.25rem'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
       {...props}
--- shared/common/src/assets/svg/SearchIcon.tsx
+++ shared/common/src/assets/svg/SearchIcon.tsx
@@ -3,9 +3,9 @@ import { SVGProps } from "react"
 const SearchIcon = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
+      width='1.5rem'
+      height='1.5rem'
+      viewBox='0 0 1.5rem 1.5rem'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
       {...props}
