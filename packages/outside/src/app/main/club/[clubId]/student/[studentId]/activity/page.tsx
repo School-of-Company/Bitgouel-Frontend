91de909a5f26dec3f2fe1c38cbb6e7dd6a62f9ed
TaerangLee <s22051@gsm.hs.kr>
2024-03-16 18:34:19 +0900

    Update app studentId folder settings, outside add studentPage

--- packages/outside/src/app/main/club/[clubId]/student/[studentId]/activity/page.tsx
+++ packages/outside/src/app/main/club/[clubId]/student/[studentId]/activity/page.tsx
@@ -2,11 +2,7 @@ import { ActivityListPage } from '@bitgouel/common/src/pages'
 import { StudentIdProps } from '@bitgouel/types'
 
 const ActivityList = ({ params }: { params: StudentIdProps }) => {
-  return (
-    <ActivityListPage
-      studentIdProps={{ clubId: params.clubId, studentId: params.studentId }}
-    />
-  )
+  return <ActivityListPage studentIdProps={params} />
 }
 
 export default ActivityList
