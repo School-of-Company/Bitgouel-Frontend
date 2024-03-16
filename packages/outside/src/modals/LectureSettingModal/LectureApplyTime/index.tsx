21a3c31c769d0e78bcf1656ca7aa08d1e97320f9
이운린 <105215297+likegitman@users.noreply.github.com>
2024-03-17 01:15:56 +0900

    Add lecture apply time

--- packages/outside/src/modals/LectureSettingModal/LectureApplyTime/index.tsx
+++ packages/outside/src/modals/LectureSettingModal/LectureApplyTime/index.tsx
@@ -1,11 +1,67 @@
-import * as S from './style';
+import {
+  LectureEndDate,
+  LectureEndTime,
+  LectureStartDate,
+  LectureStartTime,
+} from '@bitgouel/common'
+import { useSetRecoilState } from 'recoil'
+import { SearchInput } from '../style'
+import * as S from './style'
+import { ChangeEvent } from 'react'
 
 const LectureApplyTime = () => {
-    return (
-        <S.LectureApplyTimeWrapper>
-          
-        </S.LectureApplyTimeWrapper>
-    );
-};
+  const setLectureStartDate = useSetRecoilState(LectureStartDate)
+  const setLectureStartTime = useSetRecoilState(LectureStartTime)
+  const setLectureEndDate = useSetRecoilState(LectureEndDate)
+  const setLectureEndTime = useSetRecoilState(LectureEndTime)
+  return (
+    <S.LectureApplyTimeWrapper>
+      <S.TimeContainer>
+        <S.DateBox>
+          <SearchInput
+            placeholder='신청 시작일 입력 (ex: 2000.11.11)'
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setLectureStartDate(e.target.value)
+            }
+            maxLength={10}
+            style={{ width: '20.75rem' }}
+          />
+        </S.DateBox>
+        <S.DateBox>
+          <SearchInput
+            placeholder='신청 시작 시간 입력 (ex: 12:34)'
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setLectureStartTime(e.target.value)
+            }
+            maxLength={5}
+            style={{ width: '20.75rem' }}
+          />
+        </S.DateBox>
+      </S.TimeContainer>
+      <S.TimeContainer>
+        <S.DateBox>
+          <SearchInput
+            placeholder='신청 마감일 입력 (ex: 2000.11.11)'
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setLectureEndDate(e.target.value)
+            }
+            maxLength={10}
+            style={{ width: '20.75rem' }}
+          />
+        </S.DateBox>
+        <S.DateBox>
+          <SearchInput
+            placeholder='신청 마감 시간 입력 (ex: 12:34)'
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setLectureEndTime(e.target.value)
+            }
+            maxLength={5}
+            style={{ width: '20.75rem' }}
+          />
+        </S.DateBox>
+      </S.TimeContainer>
+    </S.LectureApplyTimeWrapper>
+  )
+}
 
-export default LectureApplyTime;
+export default LectureApplyTime
