69bd38efd9e279ffcade621a097c4205ea46d095
이운린 <105215297+likegitman@users.noreply.github.com>
2024-03-17 14:18:35 +0900

    Update lecture registered style

--- packages/outside/src/modals/LectureSettingModal/LectureMaxInput/style.ts
+++ packages/outside/src/modals/LectureSettingModal/LectureMaxInput/style.ts
@@ -11,25 +11,20 @@ export const MaxInputBox = styled.div<{ length: number }>`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 1.5rem;
-  height: 1.5rem;
-  background-color: ${({ theme }) => theme.color.gray['700']};
+  width: 2rem;
+  height: 2rem;
   border-radius: 0.5rem;
   padding: 0.5rem;
-
-  input[type='number']::-webkit-outer-spin-button,
-  input[type='number']::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
+  border: 0.0625rem solid ${({ theme }) => theme.color.gray['700']};
+  
   input {
+    ${({ theme }) => theme.typo.text_lg.medium};
     border: none;
     outline: none;
-    width: 1rem;
-    height: 1rem;
+    width: 2rem;
+    height: 2rem;
     background: none;
     caret-color: ${({ theme }) => theme.color.main};
-    padding-left: ${({ length }) =>
-      length === 1 ? '0.7rem' : length === 2 ? '-3.5rem' : '1rem'};
+    padding-left: ${({length}) => length === 1 ? '1.2rem' : length === 2 ? '0.8rem' : '2rem'};
   }
 `
