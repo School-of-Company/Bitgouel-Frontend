0f0cd3b3fcbb9b3710521ed68fc85f3c0dc97793
TaerangLee <s22051@gsm.hs.kr>
2024-03-18 10:26:34 +0900

    Add style modifyPage

--- packages/inside/src/PageContainer/Club/ActivityModifyPage/style.ts
+++ packages/inside/src/PageContainer/Club/ActivityModifyPage/style.ts
@@ -133,3 +133,16 @@ export const CreateButton = styled.div`
   padding: 0.85rem 2.725rem;
   border-radius: 0.5rem;
 `
+
+export const ModifyButton = styled.div<{ isAble?: boolean }>`
+  background-color: ${({ theme, isAble }) =>
+    isAble ? theme.color.main : theme.color.gray['700']};
+  color: ${({ theme, isAble }) =>
+    isAble ? theme.color.white : theme.color.gray['400']};
+  ${({ theme }) => theme.typo.text_lg};
+  cursor: pointer;
+  bottom: 1.75rem;
+  position: fixed;
+  padding: 0.85rem 2.725rem;
+  border-radius: 0.5rem;
+`
