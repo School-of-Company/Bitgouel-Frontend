5e809c7dec119642bb0bd8cd975a5a394b187926
송현우 <shw9052@naver.com>
2024-03-21 18:03:53 +0900

    Update content with props

--- shared/common/src/components/FAQItem/index.tsx
+++ shared/common/src/components/FAQItem/index.tsx
@@ -2,24 +2,20 @@
 
 import { useState } from 'react'
 import * as S from './style'
+import { FAQProps } from '@bitgouel/types'
 
-const FAQItem = () => {
+const FAQItem = ({ id, question, answer }: FAQProps) => {
   const [answerStatus, setAnswerStatus] = useState<boolean>(false)
 
   return (
     <S.FAQItemWrapper onClick={() => setAnswerStatus(!answerStatus)}>
       <S.TitleBox>
         <S.QMark>Q.</S.QMark>
-        <span>학원에서 자격증 과정을 운영할 수 있나요?</span>
+        <span>{question}</span>
       </S.TitleBox>
       <S.AnswerBox answerView={answerStatus}>
         <S.QMark>A.</S.QMark>
-        <S.Answer>
-          직업계고 중 소수의 학과가 운영되는 경우 수강인원 확보가 어려워 대학
-          연계를 지원하지 못하였습니다. 참여 학생의 수강신청을 담보할 수 없는
-          상황에서 무리한 교육과정 개설은 신청 미달 시 지역대학에게 부담이 될 수
-          있기에 이러한 결정을 내렸습니다.
-        </S.Answer>
+        <S.Answer>{answer}</S.Answer>
       </S.AnswerBox>
     </S.FAQItemWrapper>
   )
