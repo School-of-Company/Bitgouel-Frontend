5f58615917b86437a31e417425605a2ee5c43a1b
TaerangLee <s22051@gsm.hs.kr>
2024-03-23 18:29:33 +0900

    Update workspace commit test

--- shared/api/common/src/libs/queryKeys.ts
+++ shared/api/common/src/libs/queryKeys.ts
@@ -105,6 +105,6 @@ export const emailQueryKeys = {
 }
 
 export const faqQueryKeys = {
-  postQuestion: () => ['FAQ', 'post'],
   getQuestions: () => ['FAQ', 'get'],
+  postQuestion: () => ['FAQ', 'post'],
 }
