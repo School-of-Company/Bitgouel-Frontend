666d1e8b4f28b65ee9972843261b8fabb5af71bc
이운린 <105215297+likegitman@users.noreply.github.com>
2024-04-02 08:10:24 +0900

    Add outside local font

--- packages/outside/src/app/layout.tsx
+++ packages/outside/src/app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from 'next'
+import { Header } from '@bitgouel/common/src/components'
 import { GlobalLayout } from '@bitgouel/common/src/layouts'
 import '@bitgouel/common/src/styles/globals.css'
-import { Header } from '@bitgouel/common/src/components'
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
 import RecoilRootProvider from './recoilRootProvider'
 
 export const metadata: Metadata = {
@@ -12,17 +13,52 @@ export const metadata: Metadata = {
   },
 }
 
+const pretendard = localFont({
+  src: [
+    {
+      path: './fonts/woff/Pretendard-SemiBold.woff',
+      weight: '600',
+      style: 'normal'
+    },
+    {
+      path: './fonts/woff/Pretendard-Medium.woff',
+      weight: '500',
+      style: 'normal'
+    },
+    {
+      path: './fonts/woff/Pretendard-Regular.woff',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: './fonts/woff2/Pretendard-SemiBold.woff2',
+      weight: '600',
+      style: 'normal'
+    },
+    {
+      path: './fonts/woff2/Pretendard-Medium.woff2',
+      weight: '500',
+      style: 'normal'
+    },
+    {
+      path: './fonts/woff2/Pretendard-Regular.woff2',
+      weight: '400',
+      style: 'normal'
+    },
+  ],
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html>
+    <html className={pretendard.className}>
       <body>
         <RecoilRootProvider>
           <GlobalLayout>
-            <Header is_admin={true} />
+            <Header is_admin={false} />
             {children}
             <div id='modal' />
           </GlobalLayout>
