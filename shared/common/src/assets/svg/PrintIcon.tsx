7689588b6561e82e0615f2552fccabaae5d89bf4
이운린 <105215297+likegitman@users.noreply.github.com>
2024-04-08 20:05:10 +0900

    Update px to rem in svg

--- shared/common/src/assets/svg/PrintIcon.tsx
+++ shared/common/src/assets/svg/PrintIcon.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const PrintIcon = () => {
   return (
     <svg
-      width='20'
-      height='18'
+      width='1.25rem'
+      height='1.125rem'
       viewBox='0 0 20 18'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
