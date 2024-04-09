04d3aba5581c2692b164f1007cd0ed8d7e6f35e0
이운린 <105215297+likegitman@users.noreply.github.com>
2024-04-09 10:13:57 +0900

    Update list types plus PageTypes

--- shared/types/src/api/common/LectureListResponseTypes.ts
+++ shared/types/src/api/common/LectureListResponseTypes.ts
@@ -1,7 +1,8 @@
 import { LectureItemType } from './LectureItemType'
+import { PageTypes } from './PageTypes'
 
 export interface LectureListResponseTypes {
   lectures: {
     content: LectureItemType[]
-  }
+  } & PageTypes
 }
