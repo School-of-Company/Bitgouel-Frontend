f3315cd01022531e6daad693af690e357ecf1858
송현우 <shw9052@naver.com>
2024-04-15 11:46:47 +0900

    Update header import

--- shared/common/src/components/Header/index.tsx
+++ shared/common/src/components/Header/index.tsx
@@ -1,20 +1,11 @@
+'use client'
+
 import { TokenManager, useDeleteLogout } from '@bitgouel/api'
+import { Message, Question, Symbol1, Symbol2, theme } from '@bitgouel/common'
 import { usePathname, useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
-import { useRecoilState } from 'recoil'
 import { match } from 'ts-pattern'
-import {
-  Filter,
-  Message,
-  Plus,
-  Question,
-  Symbol1,
-  Symbol2,
-} from '@bitgouel/common'
-import { LectureTypeText } from '../../atoms'
-import { LectureTypeModal } from '../../modals'
-import { theme } from '../../styles'
 import * as S from './style'
 
 const menuList = [
@@ -35,9 +26,6 @@ const Header = ({ is_admin }: { is_admin: boolean }) => {
   const [borderColor, setBorderColor] = useState<string>('')
   const [spanColor, setSpanColor] = useState<string>(`${theme.color.white}`)
   const [svgView, setSvgView] = useState<string>('none')
-  const [isLectureType, setIsLectureType] = useState<boolean>(false)
-  const [lectureTypeText, setLectureTypeText] =
-    useRecoilState<string>(LectureTypeText)
   const [text, setText] = useState<string>('로그인')
   const { mutate } = useDeleteLogout()
 
@@ -143,30 +131,6 @@ const Header = ({ is_admin }: { is_admin: boolean }) => {
         </S.MenuWrapper>
         <S.ButtonWrapper view={svgView}>
           {match(pathname)
-            .with('/main/lecture', () => (
-              <>
-                <S.SelectFilterContainer>
-                  <Filter onClick={() => setIsLectureType((prev) => !prev)} />
-                  {isLectureType && (
-                    <LectureTypeModal
-                      location='헤더'
-                      text={lectureTypeText}
-                      setText={setLectureTypeText}
-                      setIsLectureType={setIsLectureType}
-                    />
-                  )}
-                </S.SelectFilterContainer>
-                {(tokenManager.authority === 'ROLE_PROFESSOR' ||
-                  tokenManager.authority === 'ROLE_GOVERNMENT' ||
-                  tokenManager.authority === 'ROLE_COMPANY_INSTRUCTOR') && (
-                  <Plus
-                    onClick={() => {
-                      push('/main/lecture/create')
-                    }}
-                  />
-                )}
-              </>
-            ))
             .with('/main/post', () => (
               <>
                 <Message onClick={() => push('/main/post/notice')} />
