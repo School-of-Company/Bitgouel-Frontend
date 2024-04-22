e06b1ff4039f83f6fc2559a4889f6e183809146f
이운린 <105215297+likegitman@users.noreply.github.com>
2024-04-22 14:10:44 +0900

    Update CommonCheckBox style

--- shared/common/src/components/CommonCheckBox/index.tsx
+++ shared/common/src/components/CommonCheckBox/index.tsx
@@ -1,35 +1,7 @@
 'use client'
 
-import styled from '@emotion/styled'
 import { CheckBoxIcon } from '../../assets'
-
-const CommonCheckboxContainer = styled.div`
-  position: relative;
-  cursor: pointer;
-
-  svg {
-    position: absolute;
-    left: 4px;
-    top: 4px;
-    cursor: pointer;
-    pointer-events: none;
-  }
-`
-
-const CommonCheckBoxStyle = styled.input`
-  width: 1.25rem;
-  height: 1.25rem;
-  margin-left: 1.2rem;
-  border: 0.0625rem solid ${({ theme }) => theme.color.gray['400']};
-  border-radius: 0.25rem;
-  background-color: ${({ theme }) => theme.color.white};
-  appearance: none;
-
-  &:checked {
-    background-color: ${({ theme }) => theme.color.main};
-    border: 0.0625rem solid ${({ theme }) => theme.color.main};
-  }
-`
+import * as S from './style'
 
 const CommonCheckBox = ({
   id,
@@ -41,14 +13,14 @@ const CommonCheckBox = ({
   onChange: (checked: boolean) => void
 }) => {
   return (
-    <CommonCheckboxContainer>
-      <CommonCheckBoxStyle
+    <S.CommonCheckboxContainer>
+      <S.CommonCheckBoxStyle
         type='checkbox'
         checked={ids?.includes(id)}
         onChange={(e) => onChange(e.target.checked)}
       />
       {ids?.includes(id) && <CheckBoxIcon />}
-    </CommonCheckboxContainer>
+    </S.CommonCheckboxContainer>
   )
 }
 
