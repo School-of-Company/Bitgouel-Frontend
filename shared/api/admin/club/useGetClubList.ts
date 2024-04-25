0e3b79aec6a34563878a55941cd3bcde5695dfab
TaerangLee <s22051@gsm.hs.kr>
2024-04-25 10:10:40 +0900

    Update instance settings test

--- shared/api/admin/club/useGetClubList.ts
+++ shared/api/admin/club/useGetClubList.ts
@@ -1,14 +1,14 @@
 import { clubQueryKeys, clubUrl, get } from '@bitgouel/api'
 import { ClubListResponseTypes } from '@bitgouel/types'
 import { UseQueryOptions, useQuery } from '@tanstack/react-query'
-import { AxiosError, AxiosResponse } from 'axios'
+import { AxiosResponse } from 'axios'
 import { ApiError } from 'next/dist/server/api-utils'
 
 export const useGetClubList = (
   queryString: string,
-  options?: UseQueryOptions<AxiosResponse>
+  options?: UseQueryOptions<ClubListResponseTypes>
 ) =>
-  useQuery<AxiosResponse<ClubListResponseTypes>, AxiosError<ApiError>>(
+  useQuery<ClubListResponseTypes, ApiError>(
     clubQueryKeys.getClub(),
     () => get(clubUrl.club(queryString)),
     options
