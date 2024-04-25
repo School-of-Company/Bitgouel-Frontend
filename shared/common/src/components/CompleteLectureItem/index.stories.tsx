ed991b7a5d11e1a96b3f1d56291b4ce46acca523
TaerangLee <s22051@gsm.hs.kr>
2024-04-25 17:09:33 +0900

    Add completeLectureItem storybook

---
+++ shared/common/src/components/CompleteLectureItem/index.stories.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import CompleteLectureItem from '.'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'common/components/CompleteLectureItem',
+  component: CompleteLectureItem,
+} as Meta<typeof CompleteLectureItem>
+
+type Story = StoryObj<typeof CompleteLectureItem>
+
+export const Primary: Story = {}
