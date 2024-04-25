db2a62ce8c6c06484d057414986e4f0b5417df6e
TaerangLee <s22051@gsm.hs.kr>
2024-04-25 17:10:17 +0900

    Add style completeLectureItem component

---
+++ shared/common/src/components/CompleteLectureItem/index.tsx
@@ -0,0 +1,27 @@
+import * as S from './style'
+
+const CompleteLectureItem = () => {
+  return (
+    <S.ItemContainer>
+      <S.ItemContentContainer>
+        <S.TitleContainer>
+          <S.TitleTypeBox>
+            <S.TitleType>상호학점인정교육과정</S.TitleType>
+          </S.TitleTypeBox>
+          <S.TitleBox>
+            <S.Title>그날 학교는 파괴적인 불꽃으로 타올랐다</S.Title>
+            <S.Line />
+          </S.TitleBox>
+        </S.TitleContainer>
+        <div>
+          <S.professorName>이태랑 교수님</S.professorName>
+        </div>
+      </S.ItemContentContainer>
+      <div>
+        <S.Date>2023년 4월 18일 이수</S.Date>
+      </div>
+    </S.ItemContainer>
+  )
+}
+
+export default CompleteLectureItem
---
+++ shared/common/src/components/CompleteLectureItem/style.ts
@@ -0,0 +1,68 @@
+import styled from '@emotion/styled'
+
+export const ItemContainer = styled.div`
+  width: 73rem;
+  height: 3.625rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 0.0625rem solid ${({ theme }) => theme.color.gray['900']};
+  padding: 0.75rem 1rem;
+`
+
+export const ItemContentContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`
+
+export const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.75rem;
+`
+
+export const TitleTypeBox = styled.div`
+  width: 9.0625rem;
+  height: 2.125rem;
+  border: 0.125rem solid ${({ theme }) => theme.color.gray['700']};
+  border-radius: 6.1875rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const TitleType = styled.span`
+  ${({ theme }) => theme.typo.text_sm.medium};
+  color: ${({ theme }) => theme.color.gray['400']};
+`
+
+export const TitleBox = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const Title = styled.span`
+  ${({ theme }) => theme.typo.text_lg.medium};
+  margin-right: 1rem;
+`
+
+export const professorName = styled.span`
+  ${({ theme }) => theme.typo.text_sm.regular};
+  color: ${({ theme }) => theme.color.gray['400']};
+`
+
+export const Line = styled.hr`
+  position: relative;
+  bottom: 0.0625rem;
+  height: 1rem;
+  width: 0.0625rem;
+  border: 0rem;
+  background-color: ${({ theme }) => theme.color.gray['700']};
+`
+
+export const Date = styled.span`
+  ${({ theme }) => theme.typo.text_sm.regular};
+  color: ${({ theme }) => theme.color.gray['400']};
+`
