4258232e665f7902ccfe34e56ed0386264ebb274
TaerangLee <s22051@gsm.hs.kr>
2024-04-30 11:42:26 +0900

    Add completeLectureTypes

---
+++ shared/types/src/common/CompleteLectureTypes.ts
@@ -0,0 +1,12 @@
+interface LectureItem {
+  id: string //UUID
+  name: string
+  lectureType: string
+  currentCompletedDate: string // LocalDate
+  lecturer: string
+  isComplete: boolean
+}
+
+export interface CompleteLectureTypes {
+  lectures: LectureItem[]
+}
