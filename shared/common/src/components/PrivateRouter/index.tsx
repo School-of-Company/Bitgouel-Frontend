51d31194ea0f48274de9582ec063b30d5ee4c00d
이운린 <105215297+likegitman@users.noreply.github.com>
2024-05-01 15:29:52 +0900

    Add PrivateRouter Component

---
+++ shared/common/src/components/PrivateRouter/index.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { TokenManager } from '@bitgouel/api';
+import { RoleEnumTypes } from '@bitgouel/types';
+import { usePathname, useRouter } from 'next/navigation';
+
+const activityPageRoles: RoleEnumTypes[] = [
+  'ROLE_ADMIN',
+  'ROLE_TEACHER',
+  'ROLE_STUDENT'
+]
+
+const createPageRoles: RoleEnumTypes[] = [
+  'ROLE_ADMIN',
+  'ROLE_PROFESSOR',
+  'ROLE_COMPANY_INSTRUCTOR',
+  'ROLE_GOVERNMENT'
+]
+
+const checkAccessCondition = (pathname: string, authority: RoleEnumTypes) => {
+  if (pathname.includes('activity')) {
+    if (!activityPageRoles.includes(authority)) {
+      return false
+    } else if (pathname.includes('write') && authority !== 'ROLE_STUDENT') {
+      return false
+    }
+  } else if ((pathname === '/main/lecture/create' || pathname === `/main/post/create`) && !createPageRoles.includes(authority)) {
+      return false
+  } else if (pathname === `/main/post/notice/create` && authority === 'ROLE_ADMIN') {
+      return false
+  }
+  return true
+}
+
+const PrivateRouter = (Component) => {
+    const Private = (props) => {
+    const { push } = useRouter()
+    const pathname = usePathname()
+    const tokenManager = new TokenManager()
+
+    if (!tokenManager.accessToken) {
+      push(`/auth/login`)
+      return null
+    } else {
+      if (tokenManager && checkAccessCondition(pathname, tokenManager.authority)) {
+        push(`/not-found`) // 임의로 redirect
+        return null
+      }
+    }
+    return <Component {...props} />
+  }
+
+  if (Component.getInitialProps) {
+    Private.getInitialProps = Component.getInitialProps
+  }
+
+  return Private
+};
+
+export default PrivateRouter;
