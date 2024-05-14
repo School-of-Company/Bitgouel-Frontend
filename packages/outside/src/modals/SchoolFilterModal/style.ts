3b877de221970d85dd145723329e3c44bec1f325
송현우 <shw9052@naver.com>
2024-05-14 10:04:45 +0900

    Update club filter stying

--- packages/outside/src/modals/SchoolFilterModal/style.ts
+++ packages/outside/src/modals/SchoolFilterModal/style.ts
@@ -2,13 +2,12 @@ import styled from '@emotion/styled'
 
 export const SchoolFilterModalWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
   align-items: center;
   width: 37.5rem;
-  height: 30rem;
+
   background-color: ${({ theme }) => theme.color.white};
   border-radius: 0.5rem;
-  border: none;
 `
 
 export const SchoolFilterTitleBox = styled.div`
@@ -16,17 +15,16 @@ export const SchoolFilterTitleBox = styled.div`
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  height: 5rem;
+  padding: 1.5rem;
+  border-bottom: 0.0625rem solid ${({ theme }) => theme.color.gray['900']};
 
   h3 {
     margin: 0;
-    margin-left: 2.2rem;
     ${({ theme }) => theme.typo.title_sm.semibold};
     color: ${({ theme }) => theme.color.black};
   }
 
   div {
-    margin-right: 1.5rem;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -36,12 +34,12 @@ export const SchoolFilterTitleBox = styled.div`
 
 export const SchoolListContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
   align-items: center;
   height: 25rem;
   overflow-y: scroll;
   overflow-x: hidden;
-  padding: 0 1rem 0 2rem;
+  justify-content: flex-start;
 
   &::-webkit-scrollbar {
     width: 0.375rem;
@@ -54,10 +52,17 @@ export const SchoolListContainer = styled.div`
 `
 
 export const SchoolItemBox = styled.div`
-  width: 33.5625rem;
-  height: 3.625rem;
+  width: 100%;
   border-bottom: 0.0625rem solid ${({ theme }) => theme.color.gray['900']};
   ${({ theme }) => theme.typo.text_lg.medium};
-  padding: 1rem 0 1rem 0;
+  padding: 1rem 2rem;
   cursor: pointer;
+
+  &:last-child {
+    border-bottom: none;
+  }
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.gray['900']};
+  }
 `
