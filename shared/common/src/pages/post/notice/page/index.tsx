18dc665cbee15328d8eb24ac43041b7a02b9296f
송현우 <shw9052@naver.com>
2024-05-20 15:20:04 +0900

    Update notice list page styling

--- shared/common/src/pages/post/notice/page/index.tsx
+++ shared/common/src/pages/post/notice/page/index.tsx
@@ -1,9 +1,15 @@
 'use client'
 
 import { useGetPostList } from '@bitgouel/api'
-import { Bg1, Message, Plus, PostItem, Question } from '@bitgouel/common'
+import {
+  Bg1,
+  Message,
+  Plus,
+  PostItem,
+  Question,
+  MainStyle,
+} from '@bitgouel/common'
 import { useRouter } from 'next/navigation'
-import * as S from './style'
 
 const NoticePage = ({ isAdmin }: { isAdmin: boolean }) => {
   const { data } = useGetPostList({
@@ -14,36 +20,36 @@ const NoticePage = ({ isAdmin }: { isAdmin: boolean }) => {
   const { push } = useRouter()
 
   return (
-    <div>
-      <S.SlideBg url={Bg1}>
-        <S.BgContainer>
-          <S.NoticeTitle>공지사항</S.NoticeTitle>
-          <S.ButtonContainer>
-            <S.NoticeButton onClick={() => push(`/main/post`)}>
+    <MainStyle.PageWrapper>
+      <MainStyle.SlideBg url={Bg1}>
+        <MainStyle.BgContainer>
+          <MainStyle.PageTitle>공지사항</MainStyle.PageTitle>
+          <MainStyle.ButtonContainer>
+            <MainStyle.SlideButton onClick={() => push(`/main/post`)}>
               <Message />
               <span>게시글</span>
-            </S.NoticeButton>
-            <S.NoticeButton onClick={() => push(`/main/post/inquiry`)}>
+            </MainStyle.SlideButton>
+            <MainStyle.SlideButton onClick={() => push(`/main/post/inquiry`)}>
               <Question />
               <span>문의사항</span>
-            </S.NoticeButton>
+            </MainStyle.SlideButton>
             {isAdmin && (
-              <S.NoticeButton onClick={() => push('/main/post/notice/create')}>
+              <MainStyle.SlideButton onClick={() => push('/main/post/notice/create')}>
                 <Plus />
                 <span>공지 추가</span>
-              </S.NoticeButton>
+              </MainStyle.SlideButton>
             )}
-          </S.ButtonContainer>
-        </S.BgContainer>
-      </S.SlideBg>
-      <S.NoticeListWrapper>
-        <S.NoticeListContainer>
+          </MainStyle.ButtonContainer>
+        </MainStyle.BgContainer>
+      </MainStyle.SlideBg>
+      <MainStyle.MainWrapper>
+        <MainStyle.MainContainer>
           {data?.posts.content.map((notice) => (
             <PostItem key={notice.id} item={notice} />
           ))}
-        </S.NoticeListContainer>
-      </S.NoticeListWrapper>
-    </div>
+        </MainStyle.MainContainer>
+      </MainStyle.MainWrapper>
+    </MainStyle.PageWrapper>
   )
 }
 
