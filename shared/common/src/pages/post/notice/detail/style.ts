647c88a2c72604c2fdfcdfbff9cdf57927e93b51
송현우 <shw9052@naver.com>
2024-05-20 15:22:41 +0900

    Update inquiry detail page styling

--- shared/common/src/pages/post/notice/detail/style.ts
+++ shared/common/src/pages/post/notice/detail/style.ts
@@ -1,42 +1,4 @@
 import styled from '@emotion/styled'
-import { StaticImageData } from 'next/image'
-
-export const SlideBg = styled.div<{ url: StaticImageData }>`
-  height: 15rem;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  background-image: url(${({ url }) => url.src});
-  background-position: center;
-  background-size: cover;
-  align-items: flex-end;
-`
-
-export const BgContainer = styled.div`
-  width: 75rem;
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 1rem;
-`
-
-export const ClubTitle = styled.span`
-  color: ${({ theme }) => theme.color.white};
-  ${({ theme }) => theme.typo.title_lg.semibold};
-`
-
-export const DocumentWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-`
-
-export const Document = styled.div`
-  width: 75rem;
-`
-
-export const TitleContainer = styled.div`
-  margin-top: 2rem;
-`
 
 export const Title = styled.span`
   ${({ theme }) => theme.typo.title_md.semibold};
