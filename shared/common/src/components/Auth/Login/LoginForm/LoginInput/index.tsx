cf91c9005ac1c5b3ab70c7eacec58cf94b565ad4
이운린 <105215297+likegitman@users.noreply.github.com>
2024-05-20 10:21:12 +0900

    Update loading state share

--- shared/common/src/components/Auth/Login/LoginForm/LoginInput/index.tsx
+++ shared/common/src/components/Auth/Login/LoginForm/LoginInput/index.tsx
@@ -1,18 +1,19 @@
 'use client'
 
-import { usePostLogin } from '@bitgouel/api'
+import { LoadingStateContext } from '@bitgouel/common'
+import { useContext } from 'react'
 import Email from './Email'
 import Password from './Password'
 import PasswordText from './PasswordText'
 import * as S from './style'
 
 const LoginInput = () => {
-  const { isLoading } = usePostLogin()
+  const loadingStateContext = useContext(LoadingStateContext)
   return (
     <S.InputWrapper>
       <S.InputContainer>
-        <Email isLoading={isLoading} />
-        <Password isLoading={isLoading} />
+        <Email isLoading={loadingStateContext} />
+        <Password isLoading={loadingStateContext} />
       </S.InputContainer>
       <PasswordText />
     </S.InputWrapper>
