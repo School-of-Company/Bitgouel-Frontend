79f1aa02e09abfe6692e13d16b1cb1ccc766537a
TaerangLee <s22051@gsm.hs.kr>
2024-05-20 13:53:29 +0900

    Update workspace commit test

--- packages/outside/src/PageContainer/Club/SchoolListPage/index.tsx
+++ packages/outside/src/PageContainer/Club/SchoolListPage/index.tsx
@@ -3,9 +3,9 @@
 import { ClubItem, SchoolFilter } from '@/components'
 import { useGetClubList } from '@bitgouel/api'
 import {
+  MainStyle,
   SchoolFilterText,
   schoolToConstants,
-  MainStyle,
 } from '@bitgouel/common'
 import { useEffect } from 'react'
 import { useRecoilValue } from 'recoil'
