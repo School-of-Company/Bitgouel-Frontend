2e65892c859239e45d20a2ee677791043f609e04
이운린 <105215297+likegitman@users.noreply.github.com>
2024-05-31 09:01:16 +0900

    Update admin after

--- shared/api/admin/src/hooks/admin/useDeleteUserWithdraw.ts
+++ shared/api/admin/src/hooks/admin/useDeleteUserWithdraw.ts
@@ -1,14 +1,13 @@
 import { adminQueryKeys, adminUrl, del } from '@bitgouel/api'
-import { useMutation } from '@tanstack/react-query'
+import { UseMutationOptions, useMutation } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
 
-export const useDeleteUserWithdraw = (userIds: string[]) =>
-  useMutation<void>(
+export const useDeleteUserWithdraw = (
+  userIds: string[],
+  options?: UseMutationOptions
+) =>
+  useMutation<void, AxiosError>(
     adminQueryKeys.deleteUserWithout(userIds),
     () => del(adminUrl.withDrawUser(userIds)),
-    {
-      onSuccess: () => window.location.reload(),
-      onError: () => {
-        console.log('error')
-      },
-    }
+    options
   )
