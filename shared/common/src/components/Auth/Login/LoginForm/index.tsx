9c9cae047b6eb82165c97ebff3c55501abec8b39
송현우 <shw9052@naver.com>
2024-06-11 20:01:14 +0900

    Update admin login redirect page

--- shared/common/src/components/Auth/Login/LoginForm/index.tsx
+++ shared/common/src/components/Auth/Login/LoginForm/index.tsx
@@ -50,7 +50,7 @@ const LoginForm = ({ isAdmin }: { isAdmin: boolean }) => {
         return toast.warning('관리자 계정으로 로그인 해주세요')
       tokenManager.setTokens(data)
       toast.success('로그인 되었습니다')
-      if (isAdmin && data.authority === 'ROLE_ADMIN') return push(`/main/admin`)
+      if (isAdmin && data.authority === 'ROLE_ADMIN') return push(`/`)
       push(`/`)
     },
     onError: ({ response }) => response && handleLoginError(response.status),
