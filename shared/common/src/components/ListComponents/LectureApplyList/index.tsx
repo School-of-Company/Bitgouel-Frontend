b6b3d2bda7f23028f7b41801cf463e9e6ad70451
송현우 <shw9052@naver.com>
2024-06-12 17:22:18 +0900

    Update lecture apply with noneResult component

--- shared/common/src/components/ListComponents/LectureApplyList/index.tsx
+++ shared/common/src/components/ListComponents/LectureApplyList/index.tsx
@@ -3,15 +3,16 @@
 import { useGetLectureApplyList } from '@bitgouel/api'
 import LectureApplyItem from '../../LectureApplyItem'
 import * as S from './style'
+import { NoneResult } from '@bitgouel/common'
 
 const LectureApplyList = ({ lectureId }: { lectureId: string }) => {
   const { data, isLoading } = useGetLectureApplyList(lectureId)
-  
+
   return (
     <S.ListContainer>
       {isLoading && <div>이수 목록을 불러오는 중...</div>}
       {data?.students && data.students.length <= 0 && (
-        <div>이수 목록이 없어요</div>
+        <NoneResult notDataTitle={'강의 신청자가'} />
       )}
       {data?.students.map((student) => (
         <LectureApplyItem
