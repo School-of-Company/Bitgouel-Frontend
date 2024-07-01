2441574ec94d3659df549d403eec89c3c5e72e13
TaerangLee <s22051@gsm.hs.kr>
2024-07-01 11:57:11 +0900

    Add useScroll hook

---
+++ shared/common/src/hooks/useScroll.tsx
@@ -0,0 +1,36 @@
+import { MutableRefObject, useEffect, useState } from 'react'
+
+interface ObserverOptions {
+  root?: Element | null // 나타내는 element
+  rootMargin?: string // margin
+  threshold?: number | number[] // 화면에 보이는 정도
+}
+
+interface UseObserverParams {
+  target: MutableRefObject<HTMLElement | null>
+  option: ObserverOptions
+}
+
+export const useObserver = ({ target, option }: UseObserverParams) => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const currentTarget = target.current
+
+    if (!currentTarget) return
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        setIsVisible(entry.isIntersecting)
+      })
+    }, option)
+
+    observer.observe(currentTarget)
+
+    return () => {
+      observer.unobserve(currentTarget)
+    }
+  }, [option, target])
+
+  return { isVisible }
+}
