eb3b4b13d690837f016c08ad9df4ddcf45cbf0a6
TaerangLee <s22051@gsm.hs.kr>
2024-07-04 15:09:43 +0900

    Add createSchoolModal component, setting storybook

---
+++ packages/outside/src/modals/CreateSchoolModal/index.stories.tsx
@@ -0,0 +1,11 @@
+import CreateSchoolModal from '.'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'outside/modals/CreateSchoolModal',
+  component: CreateSchoolModal,
+} as Meta<typeof CreateSchoolModal>
+
+type Story = StoryObj<typeof CreateSchoolModal>
+
+export const Primary: Story = {}
---
+++ packages/outside/src/modals/CreateSchoolModal/index.tsx
@@ -0,0 +1,45 @@
+import { CancelIcon, Portal, useModal } from '@bitgouel/common'
+import { SchoolItem } from '@outside/components'
+
+import * as S from './style'
+import SearchLectureType from './SearchLectureType'
+
+const CreateSchoolModal = () => {
+  const { closeModal } = useModal()
+
+  return (
+    <Portal onClose={closeModal}>
+      <S.CreateSchoolModalWrapper>
+        <S.TitleWrapper>
+          <S.TitleContainer>
+            <S.Title>새로운 학교 등록</S.Title>
+            <S.CancelIcon>
+              <CancelIcon />
+            </S.CancelIcon>
+          </S.TitleContainer>
+        </S.TitleWrapper>
+        <S.CreateSchoolModalContainer>
+          <S.SelectWrapper>
+            <S.SchoolContainer>
+              <S.Content>학교 이름</S.Content>
+              <SchoolItem placeholder='학교 이름 입력 (ex: 숭의과학기술고등학교)' />
+            </S.SchoolContainer>
+            <S.SchoolContainer>
+              <S.Content>학교 계열</S.Content>
+              <SearchLectureType />
+            </S.SchoolContainer>
+            <S.SchoolContainer>
+              <S.Content>학교 학과</S.Content>
+              <SchoolItem placeholder='쉼표로 구분해 학과 작성 (ex: 스마트 IOT과, 소프트웨어 개발과)' />
+            </S.SchoolContainer>
+            <S.SchoolContainer>
+              <S.Content>동아리</S.Content>
+            </S.SchoolContainer>
+          </S.SelectWrapper>
+        </S.CreateSchoolModalContainer>
+      </S.CreateSchoolModalWrapper>
+    </Portal>
+  )
+}
+
+export default CreateSchoolModal
