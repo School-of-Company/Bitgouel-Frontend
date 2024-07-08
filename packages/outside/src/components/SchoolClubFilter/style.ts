3b799f016cc4d5fc11a361af220251b4ac76dac0
TaerangLee <s22051@gsm.hs.kr>
2024-07-08 16:55:06 +0900

    Add schoolFilter component

---
+++ packages/outside/src/components/SchoolClubFilter/style.ts
@@ -0,0 +1,82 @@
+import styled from '@emotion/styled'
+
+export const ContSelect = styled.div`
+  position: relative;
+  justify-content: flex-end;
+`
+
+export const XIconContainer = styled.div`
+  position: absolute;
+  width: 16.1rem;
+  height: 3.5775rem;
+  align-items: center;
+  display: flex;
+  justify-content: flex-end;
+`
+
+export const SelectContainer = styled.button<{ baseColor: string }>`
+  width: 14rem;
+  height: 3.5775rem;
+  border: 1px solid ${({ theme }) => theme.color.gray['700']};
+  border-radius: 0.5rem;
+  background-color: #fff;
+  text-align: left;
+  text-indent: 0.875rem;
+  cursor: pointer;
+  outline: none;
+  position: absolute;
+  z-index: 1;
+
+  color: ${(props) => (props.baseColor === '핵심분야' ? 'gray' : 'black')};
+`
+
+export const ListMember = styled.ul`
+  position: absolute;
+  height: 122px;
+  overflow-y: auto;
+  border: 1px solid ${({ theme }) => theme.color.gray['700']};
+  border-radius: 0.5rem;
+  background-color: #fff;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  width: 14rem;
+  z-index: 2;
+
+  /* 스크롤바 설정 */
+  ::-webkit-scrollbar {
+    background-color: white;
+    border-radius: 10px;
+    width: 8px;
+    padding-right: 10px;
+    padding: 10px 0;
+  }
+
+  /* 스크롤바 썸 설정 */
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.color.gray['900']};
+    border-radius: 15px;
+    width: 5px;
+    z-index: 1;
+  }
+
+  /* 스크롤바 트랙 설정 */
+  /* ::-webkit-scrollbar-track {
+    padding: 10px 0;
+  } */
+`
+
+export const ListButton = styled.button`
+  width: 100%;
+  height: 40px;
+  border: none;
+  background-color: #fff;
+  text-align: left;
+  text-indent: 1.25rem;
+  cursor: pointer;
+  outline: none;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.gray['900']};
+  }
+`
