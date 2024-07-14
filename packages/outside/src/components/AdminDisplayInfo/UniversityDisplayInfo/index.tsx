03346be71c7a7d8237024dd2cc72204b945888a6
이운린 <105215297+likegitman@users.noreply.github.com>
2024-07-15 00:22:47 +0900

    Update UniversityDisplayInfo

--- packages/outside/src/components/AdminDisplayInfo/UniversityDisplayInfo/index.tsx
+++ packages/outside/src/components/AdminDisplayInfo/UniversityDisplayInfo/index.tsx
@@ -1,35 +1,11 @@
-import { Check, PeopleCircle } from '@bitgouel/common'
-import { ChangeEvent } from 'react'
-import * as S from './style'
-import { DisplayBarSpan, RequestDisplayBar } from '../style'
+import { DisplayBar, DisplayBarSpan } from '../style'
 
-interface Props {
-  onAll: (e: ChangeEvent<HTMLInputElement>) => void
-  onDeleteModal: () => void
-}
-
-const WithdrawDisplayInfo = ({ onAll, onDeleteModal }: Props) => {
+const UniversityDisplayInfo = () => {
   return (
-    <S.WithdrawInfoContainer>
-      <RequestDisplayBar>
-        <div>
-          <DisplayBarSpan width='1.75rem'>선택</DisplayBarSpan>
-          <DisplayBarSpan width='6rem'>이름</DisplayBarSpan>
-        </div>
-      </RequestDisplayBar>
-      <S.WithdrawButtonContainer>
-        <S.AllWithdrawBox htmlFor='allUniversity'>
-          <input type='checkbox' id='allUniversity' onChange={onAll} />
-          <PeopleCircle />
-          전체 선택
-        </S.AllWithdrawBox>
-        <S.SelectWithdrawBox onClick={onDeleteModal}>
-          <Check />
-          선택 탈퇴
-        </S.SelectWithdrawBox>
-      </S.WithdrawButtonContainer>
-    </S.WithdrawInfoContainer>
+    <DisplayBar>
+      <DisplayBarSpan width='auto'>이름</DisplayBarSpan>
+    </DisplayBar>
   )
 }
 
-export default WithdrawDisplayInfo
+export default UniversityDisplayInfo
