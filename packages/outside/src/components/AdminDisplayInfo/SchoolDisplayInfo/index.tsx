1193b48aa696759f438403f6107db9df1cdd563c
이운린 <105215297+likegitman@users.noreply.github.com>
2024-07-16 21:09:45 +0900

    Add school DisplayBar

--- packages/outside/src/components/AdminDisplayInfo/SchoolDisplayInfo/index.tsx
+++ packages/outside/src/components/AdminDisplayInfo/SchoolDisplayInfo/index.tsx
@@ -6,6 +6,8 @@ const SchoolDisplayInfo = () => {
     <DisplayBar>
       <DisplayBarSpan width='15rem'>이름</DisplayBarSpan>
       <DisplayBarSpan width='10.5rem'>계열</DisplayBarSpan>
+      <DisplayBarSpan width='10.5rem'>학과</DisplayBarSpan>
+      <DisplayBarSpan width='auto'>동아리</DisplayBarSpan>
     </DisplayBar>
   )
 }
