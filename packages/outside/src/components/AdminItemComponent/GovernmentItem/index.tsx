88d2efc9235f94b393b69e38129d07b6c0ad72cb
이운린 <105215297+likegitman@users.noreply.github.com>
2024-07-22 01:28:01 +0900

    Add GovernmentItem

---
+++ packages/outside/src/components/AdminItemComponent/GovernmentItem/index.tsx
@@ -0,0 +1,57 @@
+import {
+    governmentQueryKeys,
+    useDeleteUniversity
+} from '@bitgouel/api'
+import { AppropriationModal, useModal } from '@bitgouel/common'
+import { useQueryClient } from '@tanstack/react-query'
+import { toast } from 'react-toastify'
+import CompoundAdminItemComponent from '../CompoundAdminItemComponent'
+
+interface Props {
+  governmentId: string
+  name: string
+  nameWidth: string
+}
+
+const GovernmentItem = ({ governmentId, name, nameWidth }: Props) => {
+  const { openModal, closeModal } = useModal()
+  const queryClient = useQueryClient()
+  const onSuccess = (message: string) => {
+    closeModal()
+    queryClient.invalidateQueries(governmentQueryKeys.getGovernment())
+    toast.success(message)
+  }
+
+  const { mutate: deleteGovernment } = useDeleteUniversity(governmentId, {
+    onSuccess: () => onSuccess('유관기관을 삭제하였습니다.'),
+  })
+
+  const onDeleteUniversity = () =>
+    openModal(
+      <AppropriationModal
+        isApprove={false}
+        question='대학을 삭제하시겠습니까?'
+        purpose='삭제하기'
+        title={name}
+        onAppropriation={(callbacks) => deleteGovernment(undefined, callbacks)}
+      />
+    )
+
+  return (
+    <CompoundAdminItemComponent>
+      <CompoundAdminItemComponent.AdminItemContainer gap='1.5rem'>
+        <CompoundAdminItemComponent.AdminItemName
+          name={name}
+          nameWidth={nameWidth}
+        />
+        <CompoundAdminItemComponent.ControlButton
+          isModify={true}
+          isDelete={true}
+          onDelete={onDeleteUniversity}
+        />
+      </CompoundAdminItemComponent.AdminItemContainer>
+    </CompoundAdminItemComponent>
+  )
+}
+
+export default GovernmentItem
