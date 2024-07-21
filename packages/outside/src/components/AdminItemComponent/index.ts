e39943c7001e76823aa6b9773326b715f1c7167a
이운린 <105215297+likegitman@users.noreply.github.com>
2024-07-22 01:33:16 +0900

    Add GovernmentList

--- packages/outside/src/components/AdminItemComponent/index.ts
+++ packages/outside/src/components/AdminItemComponent/index.ts
@@ -1,2 +1,3 @@
-export { default as UniversityItem } from './UniversityItem'
 export { default as CompoundItemComponent } from './CompoundAdminItemComponent'
+export { default as GovernmentItem } from './GovernmentItem'
+export { default as UniversityItem } from './UniversityItem'
---
+++ packages/outside/src/components/GovernmentList/index.tsx
@@ -0,0 +1,32 @@
+import { useGetGovernment } from '@bitgouel/api'
+import { NoneResult, WaitingAnimation } from '@bitgouel/common'
+import { UniversityDisplayInfo } from '../AdminDisplayInfo'
+import { GovernmentItem } from '../AdminItemComponent'
+import { UserListContainer } from '../NewUserList/style'
+
+const GovernmentList = () => {
+  const { data, isLoading } = useGetGovernment()
+
+  return (
+    <>
+      <UniversityDisplayInfo />
+      <UserListContainer>
+        {isLoading && <WaitingAnimation title={'대학 명단을'} />}
+        {data?.governments.length <= 0 ? (
+          <NoneResult title={'대학 명단이'} />
+        ) : (
+          data?.governments.map((government) => (
+            <GovernmentItem
+              key={government.id}
+              governmentId={String(government.id)}
+              name={government.governmentName}
+              nameWidth='32.5rem'
+            />
+          ))
+        )}
+      </UserListContainer>
+    </>
+  )
+}
+
+export default GovernmentList
