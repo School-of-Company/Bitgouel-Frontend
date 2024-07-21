1435548cda7c318bfd569c4bc71d89bb8c1bf7b2
이운린 <105215297+likegitman@users.noreply.github.com>
2024-07-22 00:42:04 +0900

    Add CompanyPage CompanyList Component

---
+++ packages/outside/src/components/CompanyList/index.tsx
@@ -0,0 +1,32 @@
+import { useGetCompany } from '@bitgouel/api'
+import { NoneResult, WaitingAnimation } from '@bitgouel/common'
+import { UniversityDisplayInfo } from '../AdminDisplayInfo'
+import { CompanyItem } from '../AdminItemComponent'
+import { UserListContainer } from '../NewUserList/style'
+
+const CompanyList = () => {
+  const { data, isLoading } = useGetCompany()
+
+  return (
+    <>
+      <UniversityDisplayInfo />
+      <UserListContainer>
+        {isLoading && <WaitingAnimation title={'기업 명단을'} />}
+        {data?.companies.length <= 0 ? (
+          <NoneResult title={'기업 명단이'} />
+        ) : (
+          data?.companies.map((company) => (
+            <CompanyItem
+              key={company.id}
+              companyId={String(company.id)}
+              name={company.name}
+              nameWidth='32.5rem'
+            />
+          ))
+        )}
+      </UserListContainer>
+    </>
+  )
+}
+
+export default CompanyList
