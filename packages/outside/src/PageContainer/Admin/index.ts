0a5c5059d9453ea83936d80c942c5436269f30a2
이운린 <105215297+likegitman@users.noreply.github.com>
2024-07-22 01:34:43 +0900

    Add goverment apps

--- packages/outside/src/PageContainer/Admin/index.ts
+++ packages/outside/src/PageContainer/Admin/index.ts
@@ -1,4 +1,5 @@
-export { default as UserListPage } from './UserListPage'
+export { default as GovernmentPage } from './GovernmentPage'
 export { default as NewUserListPage } from './NewUserListPage'
-export { default as WithdrawUserListPage } from './WithdrawUserListPage'
 export { default as UniversityPage } from './UniversityPage'
+export { default as UserListPage } from './UserListPage'
+export { default as WithdrawUserListPage } from './WithdrawUserListPage'
