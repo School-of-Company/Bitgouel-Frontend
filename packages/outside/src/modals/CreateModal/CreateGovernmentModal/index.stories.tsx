b9df6ebcf23d1575e059337f4857410abec2ab82
이운린 <105215297+likegitman@users.noreply.github.com>
2024-07-28 01:56:08 +0900

    Add CreateGovernmentModal

---
+++ packages/outside/src/modals/CreateModal/CreateGovernmentModal/index.stories.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Meta, StoryObj } from '@storybook/react'
+import CreateGovernmentModal from '.'
+
+export default {
+  title: 'outside/modals/CreateGovernmentModal',
+  component: CreateGovernmentModal,
+} as Meta<typeof CreateGovernmentModal>
+
+type Story = StoryObj<typeof CreateGovernmentModal>
+
+export const Primary: Story = {}
---
+++ packages/outside/src/modals/CreateModal/CreateGovernmentModal/index.tsx
@@ -0,0 +1,94 @@
+'use client'
+
+import { usePostGovernment } from '@bitgouel/api'
+import { CancelIcon, FieldEnum, Portal, useModal } from '@bitgouel/common'
+import { SchoolInputItem } from '@outside/components'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { useRecoilValue } from 'recoil'
+import {
+    CancelIconBox,
+    Content,
+    CreateModalContainer,
+    CreateModalWrapper,
+    SelectContainer,
+    SelectWrapper,
+    SubmitButton,
+    SubmitContainer,
+    Title,
+    TitleWrapper,
+} from '../style'
+import SearchFieldEnum from './SearchFieldEnum'
+
+const CreateGovernmentModal = () => {
+  const [governmentName, setGovernmentName] = useState<string>('')
+  const fieldEnum = useRecoilValue(FieldEnum)
+  const [isDisabled, setIsDisabled] = useState<boolean>(false)
+
+  const { closeModal } = useModal()
+
+  const { mutate } = usePostGovernment({
+    onSuccess: () => {
+      toast.success('유관기관이 등록되었습니다.')
+      closeModal()
+    },
+    onError: () => {
+      toast.error('유관기관 등록에 실패하였습니다.')
+    },
+  })
+
+  const onCreate = async () => {
+    setIsDisabled(true)
+    try {
+      await new Promise((resolve, reject) =>
+        setTimeout(() => {
+          mutate(
+            { governmentName, field: fieldEnum },
+            {
+              onSuccess: resolve,
+              onError: reject,
+            }
+          )
+        }, 2000)
+      )
+    } finally {
+      setIsDisabled(false)
+    }
+  }
+
+  return (
+    <Portal onClose={closeModal}>
+      <CreateModalWrapper>
+        <TitleWrapper>
+          <Title>새로운 유관기관 등록</Title>
+          <CancelIconBox onClick={closeModal}>
+            <CancelIcon />
+          </CancelIconBox>
+        </TitleWrapper>
+        <CreateModalContainer>
+          <SelectWrapper>
+            <SelectContainer>
+              <Content>유관기관 이름</Content>
+              <SchoolInputItem
+                placeholder='유관기관 이름 입력 (ex: 숭의과학기술고등학교)'
+                type='유관기관 이름'
+                onChange={(value) => setGovernmentName(value)}
+              />
+            </SelectContainer>
+            <SelectContainer>
+              <Content>유관기관 분야</Content>
+              <SearchFieldEnum />
+            </SelectContainer>
+            <SubmitContainer>
+              <SubmitButton disabled={isDisabled} onClick={onCreate}>
+                유관기관 등록
+              </SubmitButton>
+            </SubmitContainer>
+          </SelectWrapper>
+        </CreateModalContainer>
+      </CreateModalWrapper>
+    </Portal>
+  )
+}
+
+export default CreateGovernmentModal
