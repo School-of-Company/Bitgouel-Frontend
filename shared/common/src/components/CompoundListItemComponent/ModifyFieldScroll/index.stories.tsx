10dc2464fc092dd16f912c67182307d6ce3dc099
송현우 <shw9052@naver.com>
2024-08-04 19:27:00 +0900

    Add fieldScroll modal storybook

---
+++ shared/common/src/components/CompoundListItemComponent/ModifyFieldScroll/index.stories.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import ModifyFieldScroll from '.'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'common/components/CompoundListItemComponent/ModifyFieldScroll',
+  component: ModifyFieldScroll,
+} as Meta<typeof ModifyFieldScroll>
+
+type Story = StoryObj<typeof ModifyFieldScroll>
+
+export const Primary: Story = {}
