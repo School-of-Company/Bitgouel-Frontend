1938b16682f5932cca13fe0c408938b4f0e4af76
이운린 <105215297+likegitman@users.noreply.github.com>
2024-08-11 19:26:44 +0900

    Update excel data any -> unknown

--- shared/common/src/utils/excelDownload.ts
+++ shared/common/src/utils/excelDownload.ts
@@ -2,7 +2,7 @@ import { saveAs } from 'file-saver'
 import { toast } from 'react-toastify'
 
 interface Parameter {
-  data: any
+  data: unknown
   fileName: string
   fileExtension: 'xlsx'
 }
@@ -13,11 +13,13 @@ const fileTypes = {
 
 const excelDownload = ({ data, fileName, fileExtension }: Parameter) => {
   if (!data) return toast.error('다운로드 버튼을 다시 눌러주세요.')
-    
-  const fileBlob: Blob = new Blob([data], {
-    type: fileTypes[fileExtension],
-  })
-  saveAs(fileBlob, `${fileName}.${fileExtension}`)
+
+  if (data instanceof ArrayBuffer || data instanceof Uint8Array) {
+    const fileBlob: Blob = new Blob([data], {
+      type: fileTypes[fileExtension],
+    })
+    saveAs(fileBlob, `${fileName}.${fileExtension}`)
+  }
 }
 
 export default excelDownload
