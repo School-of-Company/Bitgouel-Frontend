a0c28e33ec0a3bb2806feae2e0d7c00885888072
이운린 <105215297+likegitman@users.noreply.github.com>
2024-08-29 21:01:27 +0900

    Add useFileUpload custom hooks

---
+++ shared/common/src/hooks/useFileUpload.tsx
@@ -0,0 +1,22 @@
+import React, { ChangeEvent, useCallback } from 'react'
+
+const useFileUpload = (upload) => {
+  const onFileUpload = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const excelFile: File | null = e.currentTarget.files
+        ? e.currentTarget.files[0]
+        : null
+
+      const formData = new FormData()
+      formData.append('file', excelFile ?? '')
+      upload(formData)
+
+      e.currentTarget.value = ''
+    },
+    [upload]
+  )
+
+  return { onFileUpload }
+}
+
+export default useFileUpload
